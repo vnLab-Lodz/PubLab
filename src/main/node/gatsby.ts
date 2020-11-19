6756edf64a16c56e3d5a6933a86be9e86f3b39dd
Piotr Kaźmierski <217847@edu.p.lodz.pl>
2020-11-19 13:11:04 +0100

    Changed Winston to simple file logging, fixed bugs
    Uses fs.createWriteStream in append mode.
    Fixed problem with checkForGatsby Promise being rejected when gatsby-cli is not installed.
    Added try..catch around JSON parsing.

--- src/main/node/gatsby.ts
+++ src/main/node/gatsby.ts
@@ -1,35 +1,32 @@
 import { exec } from 'child_process';
-import * as winston from 'winston';
+import * as fs from 'fs';
 
-const myFormat = winston.format.printf(
-  ({ level, message, label, timestamp }) => {
-    return `${timestamp} ${level}: ${message}`;
-  }
-);
+const logger = fs.createWriteStream('gatsby-cli-install.log', { flags: 'a' });
+const tzOffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
 
-const logger = winston.createLogger({
-  format: winston.format.combine(winston.format.timestamp(), myFormat),
-  transports: [
-    new winston.transports.File({ filename: 'logs/gatsby-cli-install.log' }),
-  ],
-});
+function writeLog(msg: string): void {
+  let localISOTime = new Date(Date.now() - tzOffset).toISOString();
+  logger.write(localISOTime.replace(/T/, ' ').replace(/\..+/, '') + ' ' + msg + '\n');
+}
 
 /**
  * Check if gatsby-cli is installed globally.
+ * Rejects the promise if a command execution error occurs or JSON cannot be parsed.
  * @return {Promise<boolean>}
  */
 function checkForGatsby(): Promise<boolean> {
   return new Promise<boolean>((resolve, reject) => {
     exec('npm list -g gatsby-cli --json', (error, stdout, stderr) => {
-      if (error) reject(error);
+      if (error && stdout.trim() !== '{}') reject(error);
 
-      let resultObj: any = JSON.parse(stdout);
+      let resultObj: any;
+      try {
+        resultObj = JSON.parse(stdout);
+      } catch (e) {
+        reject(stdout);
+      }
 
-      if (
-        Object.keys(resultObj).length === 0 &&
-        resultObj.constructor === Object
-      )
-        resolve(false);
+      if (Object.keys(resultObj).length === 0 && resultObj.constructor === Object) resolve(false);
       else resolve(true);
     });
   });
@@ -37,6 +34,7 @@ function checkForGatsby(): Promise<boolean> {
 
 /**
  * Checks if gatsby is already installed globally and if not installs it.
+ * Rejects the promise if a command execution error occurs or JSON cannot be parsed.
  * @return {Promise<void>}
  */
 export function installGatsby(): Promise<void> {
@@ -44,16 +42,16 @@ export function installGatsby(): Promise<void> {
     checkForGatsby().then(
       (value) => {
         if (value) {
-          logger.info('gatsby-cli is already installed. Skipping installation');
+          writeLog('gatsby-cli is already installed. Skipping installation');
           resolve();
         } else {
-          logger.info('gatsby-cli not installed. Installing...');
+          writeLog('gatsby-cli not installed. Installing...');
           exec('npm install -g gatsby-cli --quiet', (error, stdout, stderr) => {
             if (error) reject(error);
 
-            logger.info(`package installer standard output: ${stdout}`);
-            logger.error(`package installer error output: ${stderr}`);
-            logger.info('Finished installation of gatsby-cli');
+            writeLog(`package installer standard output: ${stdout}`);
+            writeLog(`package installer error output: ${stderr}`);
+            writeLog('Finished installation of gatsby-cli');
             resolve();
           });
         }
