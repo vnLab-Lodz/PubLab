7b08d453a24d89a6d1377907dc4d9ebdb5b17e6c
emiliamarkowska <emarkowska10@gmail.com>
2020-11-25 20:53:27 +0100

    client id and client secret hidden with dotenv

--- src/renderer/components/app/app.tsx
+++ src/renderer/components/app/app.tsx
@@ -4,10 +4,13 @@ import '../description/description';
 import Description from '../description/description';
 import { Provider } from 'react-redux';
 import { configStore } from '../../../shared/configureStore';
-import {postAccessToken, authorizeWithGithub, createNewRepository, getUserRepositories} from "../../../main/git/gitAuthorization";
+import {postAccessToken, authorizeWithGithub} from "../../../main/git/gitAuthorization";
 import ReadOnlyDict = NodeJS.ReadOnlyDict;
 import {clone} from "../../../main/git/gitOperations";
 
+require('dotenv').config();
+
+console.log(process.env);
 const store = configStore('renderer');
 store.subscribe(() => console.log('action received in renderer'));
 
@@ -18,10 +21,15 @@ type AppProps = {
 class App extends React.Component <AppProps>{
 
     async componentDidMount(): Promise<void> {
+        /** piece of code to get code after authorizing, code is a param of main page after making callback,
+        *   then having code it is exchanged for access token
+
+         */
         let search = window.location.search;
         let codeString = search.substr(1, 4);
         if(codeString == "code"){
             let accessToken = await postAccessToken(search);
+            //place to save it in a slice
             console.log(accessToken);
         }
     }
@@ -31,7 +39,6 @@ class App extends React.Component <AppProps>{
             <Provider store={store}>
                 <h1 className='hello'>Hello World!</h1>
                 <button onClick={()=>authorizeWithGithub()}> Odpal git</button>
-                <button onClick={()=>getUserRepositories('as')}> tworz repo</button>
                 <button onClick={()=>clone("C:/vnlab-tool", "https://github.com/jedrekszor/vnlab-test")}> Clone </button>
                 <Description />
             </Provider>
