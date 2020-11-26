19d1a3f0cd415e4ab248cf7232fc06b919cda416
Piotrek Karczewski <p.karczewsk@gmail.com>
2020-11-26 20:00:57 +0100

    Clean up description

--- src/renderer/components/RouterComponents/Description/Description.tsx
+++ src/renderer/components/RouterComponents/Description/Description.tsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import './Description.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import {
-  example,
-  exampleInMain,
-  exampleLocal,
-  selectCurrentUser,
-} from '../../../../shared/slices/currentUserSlice';
+import { selectCurrentUser } from '../../../../shared/slices/currentUserSlice';
 import {
   installGatsbyCLI,
   selectGatsbyInstallStatus,
@@ -26,37 +21,6 @@ const Description = () => {
     <div>
       <h1 className='hello'>Hello World!</h1>
       <p className='description'>Welcome to your Electron application.</p>
-      <button
-        onClick={() => dispatch(example({ nick: 'currentUser from renderer' }))}
-      >
-        Dispatch Example action
-      </button>
-      <button
-        onClick={() =>
-          dispatch(
-            exampleLocal({
-              data: {
-                nick: 'all of currentUser from renderer',
-              },
-            })
-          )
-        }
-      >
-        Dispatch Example local action
-      </button>
-      <button
-        onClick={() =>
-          dispatch(
-            exampleInMain({
-              data: {
-                nick: 'currentUser from action executed only in main process',
-              },
-            })
-          )
-        }
-      >
-        Dispatch Example action in main process
-      </button>
       <button onClick={() => dispatch(installGatsbyCLI())}>
         Install gatsby-cli
       </button>
