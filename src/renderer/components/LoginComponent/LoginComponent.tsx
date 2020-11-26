49bde10a843fc6b950b7f452e43be19fbd5dc4f6
unknown <szymonrucinski@icloud.com>
2020-11-26 19:39:14 +0100

    authenticate-screen done

--- src/renderer/components/LoginComponent/LoginComponent.tsx
+++ src/renderer/components/LoginComponent/LoginComponent.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { FaGithub } from 'react-icons/fa';
 import './LoginComponent.scss';
 
 const LoginComponent = () => {
 
   return (
-    <div>LoginComponent</div>
+    <div className="container">
+    <div>Welcome to VNlab</div>
+    <div>
+    <button className="button"onClick={()=>{console.log('Prompt new browser window with GitHub auth.')}}>Login to GitHub <FaGithub />
+</button>
+    </div>
+    </div>
   );
 };
 
