265044302e2ae3070526505d0303e1238701e40c
Piotrek Karczewski <p.karczewsk@gmail.com>
2020-11-26 20:03:13 +0100

    Implement basic UserLogin wrapper functionality

--- src/renderer/components/LoginComponent/LoginComponent.tsx
+++ src/renderer/components/LoginComponent/LoginComponent.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { authorizeGitHubUserAsync } from '../../../shared/slices/currentUserSlice';
 import './LoginComponent.scss';
 
 const LoginComponent = () => {
+  const dispatch = useDispatch();
 
   return (
-    <div>LoginComponent</div>
+    <>
+      <div>LoginComponent</div>
+      <button
+        onClick={() => {
+          dispatch(authorizeGitHubUserAsync(process.env.GITHUB_CLIENT_ID));
+        }}
+      >
+        LOG IN
+      </button>
+    </>
   );
 };
 
--- src/renderer/components/UserLogin/UserLogin.tsx
+++ src/renderer/components/UserLogin/UserLogin.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
-import './UserLogin.scss';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  requestAccesTokenAsync,
+  selectCurrentUser,
+} from '../../../shared/slices/currentUserSlice';
 import LoginComponent from '../LoginComponent/LoginComponent';
+import './UserLogin.scss';
 
-const UserLogin = ({children} : any) => {
+const UserLogin = ({ children }: any) => {
+  const dispatch = useDispatch();
+  const currentUser = useSelector(selectCurrentUser);
 
-  const isUserLoggedIn = () => {
-    //TODO: check if user is logged in
-    return true;
-  }
+  useEffect(() => {
+    dispatch(
+      requestAccesTokenAsync({
+        clientId: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET,
+        code: currentUser.auth.code,
+      })
+    );
+  }, [currentUser.auth.code]);
+
+  //TODO: refactor this to check if user data has been fetched @ProudBloom
+  const isUserLoggedIn = currentUser.auth.accessToken !== null;
 
   const getRenderedComponent = () => {
-      return isUserLoggedIn() ? <>{children}</> : <LoginComponent/>;
-  }
-  return (
-      getRenderedComponent()
-  );
+    return isUserLoggedIn ? <>{children}</> : <LoginComponent />;
+  };
+  return getRenderedComponent();
 };
 
 export default UserLogin;
