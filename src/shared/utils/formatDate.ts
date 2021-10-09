63327909fd92e3fbea8639f7f7b6e46ef5862fbc
Piotr Karczewski <p.karczewsk@gmail.com>
2021-10-09 21:49:36 +0200

    refactor: :art: Refactor formatDate util
    Changed the names of the params and variables to make the code more readible. Used a template literal to save a few characters.

--- src/shared/utils/formatDate.ts
+++ src/shared/utils/formatDate.ts
@@ -1,7 +1,8 @@
-export const formatDate = (date_ob: string) => {
-  const d = new Date(date_ob);
-  const date = ('0' + d.getDate()).slice(-2);
-  const month = ('0' + (d.getMonth() + 1)).slice(-2);
-  const year = d.getFullYear();
-  return `${year}-${month}-${date}`;
-};
+export function formatDate(dateStr: string): string {
+  const date = new Date(dateStr);
+  const day = `0${date.getDate()}`.slice(-2);
+  const month = `0${date.getMonth() + 1}`.slice(-2);
+  const year = date.getFullYear();
+
+  return `${year}-${month}-${day}`;
+}
