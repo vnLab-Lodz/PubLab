384f9ac7e66c1aa62e8d2564d222b789509803ed
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-10-20 21:21:35 +0200

    Loaded HK Grotesk in ThemeProvider

--- src/renderer/theme.ts
+++ src/renderer/theme.ts
@@ -1,16 +1,19 @@
-import { HKGrotesk } from './fonts/HKGrotesk/HKGrotesk-Black.woff2';
-import { createTheme } from "@mui/material";
+import { createTheme } from '@mui/material';
+import HKGrotesk from './fonts/HKGrotesk/HKGrotesk-SemiBold.woff2';
 
 export const theme = createTheme({
   typography: {
-    fontFamily: 'HKGrotesk',
+    fontFamily: 'HK Grotesk',
   },
   components: {
     MuiCssBaseline: {
       styleOverrides: `
         @font-face {
-          font-family: 'HKGrotesk';
-          src: local('HKGrotesk'), local('HKGrotesk-Black'), url(${HKGrotesk}) format('woff2');
+          font-family: 'HK Grotesk';
+          font-style: normal;
+          font-display: swap;
+          font-weight: 600;
+          src: local('HK Grotesk SemiBold'), local('HKGrotesk-SemiBold'), url(${HKGrotesk}) format('woff2');
           unicodeRange: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF;
         }
       `,
