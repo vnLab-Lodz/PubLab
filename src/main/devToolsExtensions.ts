e4938d185dd0145150c31d3a08b801d0648b73dc
LostInStatic <kowal.a1234@gmail.com>
2021-10-22 16:48:36 +0200

    Set up dev tools extensions installation

---
+++ src/main/devToolsExtensions.ts
@@ -0,0 +1,8 @@
+import installExtension, { REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS} from 'electron-devtools-installer';
+
+
+export default async function installDevToolsExtensions(): Promise<void> {
+    await installExtension([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS])
+        .then(() => console.log('Dev tools extensions installed successfully'))
+        .catch((err) => console.log('An error occurred while installing dev tools extensions: ', err));
+};
--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 import { configStore } from '../shared/redux/configureStore';
+import installDevToolsExtensions from './devToolsExtensions';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: any;
 
@@ -34,7 +35,7 @@ const createWindow = (): void => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', () => installDevToolsExtensions().then(createWindow));
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
