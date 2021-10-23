c60ddc542727fa96a59a4d8d38d288fa0dbc2d62
LostInStatic <kowal.a1234@gmail.com>
2021-10-24 00:29:41 +0200

    Skip dev tools extensions install in production

--- src/main/index.ts
+++ src/main/index.ts
@@ -35,7 +35,12 @@ const createWindow = async (): Promise<void> => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', () => installDevToolsExtensions().then(createWindow));
+app.on('ready', async () => {
+  if (!app.isPackaged) {
+    await installDevToolsExtensions();
+  }
+  createWindow();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
