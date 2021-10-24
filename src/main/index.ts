7787e4a4bca0f10d6363c7969e82ae10743baa64
LostInStatic <kowal.a1234@gmail.com>
2021-10-24 10:36:29 +0200

    Dev tools extensions (#86)
    * Added electron-devtools-installer
    
    * Set up dev tools extensions installation
    
    * Ensure dev tools open after main window is ready
    
    Helps avoid issues with extensions not initialising properly
    
    * Prettified Code!
    
    * Skip dev tools extensions install in production
    
    Co-authored-by: LostInStatic <LostInStatic@users.noreply.github.com>

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,5 +1,6 @@
 import { app, BrowserWindow } from 'electron';
 import { configStore } from '../shared/redux/configureStore';
+import installDevToolsExtensions from './devToolsExtensions';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: any;
 
@@ -12,7 +13,7 @@ if (require('electron-squirrel-startup')) {
 
 const store = configStore('main');
 
-const createWindow = (): void => {
+const createWindow = async (): Promise<void> => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     height: 600,
@@ -25,7 +26,7 @@ const createWindow = (): void => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
+  await mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY);
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
@@ -34,7 +35,12 @@ const createWindow = (): void => {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', async () => {
+  if (!app.isPackaged) {
+    await installDevToolsExtensions();
+  }
+  createWindow();
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
