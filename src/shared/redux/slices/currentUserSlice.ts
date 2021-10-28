6be730f31dd25af5b43253ad2bf2b4bc94d1aebb
LostInStatic <kowal.a1234@gmail.com>
2021-10-29 00:40:35 +0200

    unused import cleanup

--- src/shared/redux/slices/currentUserSlice.ts
+++ src/shared/redux/slices/currentUserSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import {
   authorizeWithGithub,
   requestAccessToken,
