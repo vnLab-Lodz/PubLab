244e74e8bc5326ff566d4bc7e36e8cc329b90030
Mateusz Kmieciak <mkmieciak70@gmail.com>
2021-10-28 19:16:52 +0200

    Node - node installation for Windows

--- src/main/node/nodeCheck.ts
+++ src/main/node/nodeCheck.ts
@@ -19,3 +19,28 @@ export function checkForNode(): Promise<boolean> {
     });
   });
 }
+
+/**
+ * Checks if node is already installed globally and if not installs it.
+ * Rejects the promise if a command execution error occurs or node verification fails.
+ * @return {Promise<void>}
+ */
+export function installNode(): Promise<void> {
+  return new Promise<void>((resolve, reject) => {
+    appendLog('Installing node...');
+
+    checkForNode()
+      .then(_ => {
+        let version = 'v17.0.1';
+        exec(`msiexec.exe /i https://nodejs.org/dist/${version}/node-${version}-x64.msi /quiet`, (error, stdout, stderr) => {
+          if (error) reject(error);
+          appendLog(`Launched installation of node. Version: ${version}`);
+          resolve();
+        });
+      })
+      .catch(error => {
+        appendLog('Cannot install node - an error occurred during verification of its installation.');
+        resolve(error);
+      });
+  });
+}
