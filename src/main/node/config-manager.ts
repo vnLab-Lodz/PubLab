37bc5a0230cda129b0ce04ca40e4a33153652b1e
Juliusz <juliusz.kusiak@gmail.com>
2021-10-30 01:32:30 +0200

    Change collaborator deletion to only by id, test the solution

--- src/main/node/config-manager.ts
+++ src/main/node/config-manager.ts
@@ -4,7 +4,6 @@ const fs = require('fs');
 
 class Collaborator {
   username: string;
-
   role: string;
 
   constructor(username: string, role: string) {
--- src/shared/redux/slices/publicationsSlice.ts
+++ src/shared/redux/slices/publicationsSlice.ts
@@ -1,7 +1,8 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, current, PayloadAction } from '@reduxjs/toolkit';
 import { RootState } from '../rootReducer';
 
 type Collaborator = {
+  id: string;
   githubUsername: string;
   role: string;
 };
@@ -11,11 +12,15 @@ type Publication = {
   dirPath: string;
   publicationName: string;
   description: string;
-  collaborators: [Collaborator];
+  collaborators: Collaborator[];
   packageManager: string;
   useSass: string,
   useTypescript: string,
 };
+
+type PublicationList = {
+  list: Publication[];
+}
 
 // type created to exclude collaborator type to avoid an issue with
 // index access writes on union of keys, which would complicate the
@@ -34,69 +39,69 @@ type PublicationModification = {
 // publication collaborators
 type CollaboratorListModification = {
   pubId: string;
-  value: Collaborator;
+  value: Collaborator | string;
 };
 
 
-const initialState: [Publication] = [
-  {
+const initialState: PublicationList = {
+  list: [{
     id: '',
     dirPath: '',
     publicationName: '',
     description: '',
-    collaborators: [{ githubUsername: '', role: '' }],
+    collaborators: [{ id: '', githubUsername: '', role: '' }],
     packageManager: '',
     useSass: 'false',
     useTypescript: 'false',
-  },
-];
+  }],
+};
 
 const publicationsSlice = createSlice({
   name: 'publications',
   initialState,
   reducers: {
     setPublicationsList: (
-      state: [Publication],
-      action: PayloadAction<[Publication]>
+      state: PublicationList,
+      action: PayloadAction<PublicationList>
     ) => {
       state = action.payload;
     },
     addPublication: (
-      state: [Publication],
+      state: PublicationList,
       action: PayloadAction<Publication>
     ) => {
-      state.push(action.payload);
+      state.list.push(action.payload);
     },
     deletePublication: (
-      state: [Publication],
+      state: PublicationList,
       action: PayloadAction<string>
     ) => {
-      const newState = state.filter(publication => publication.id !== action.payload) as [Publication];
-      state = newState;
+      const updatedPublications = state.list.filter(publication => publication.id !== action.payload);
+      state.list = updatedPublications;
     },
     setPublicationField: (
-      state: [Publication],
+      state: PublicationList,
       action: PayloadAction<PublicationModification>
     ) => {
-      const chosenPubIndex = state.findIndex(publication => publication.id === action.payload.id);
-      state[chosenPubIndex][action.payload.field] = action.payload.value;
+      const chosenPubIndex = state.list.findIndex(publication => publication.id === action.payload.id);
+      state.list[chosenPubIndex][action.payload.field] = action.payload.value;
     },
     addCollaborator: (
-      state: [Publication],
+      state: PublicationList,
       action: PayloadAction<CollaboratorListModification>
     ) => {
-      const chosenPubIndex = state.findIndex(publication => publication.id === action.payload.pubId);
+      const chosenPubIndex = state.list.findIndex(publication => publication.id === action.payload.pubId);
       const collaborator = action.payload.value as Collaborator;
-      state[chosenPubIndex].collaborators.push(collaborator);
+      state.list[chosenPubIndex].collaborators.push(collaborator);
     },
     deleteCollaborator: (
-      state: [Publication],
+      state: PublicationList,
       action: PayloadAction<CollaboratorListModification>
     ) => {
-      const chosenPubIndex = state.findIndex(publication => publication.id === action.payload.pubId);
-      const collaborator = action.payload.value;
-      const updatedCollaborators = state[chosenPubIndex].collaborators.filter(element => element !== collaborator) as [Collaborator];
-      state[chosenPubIndex].collaborators = updatedCollaborators;
+      const chosenPubIndex = state.list.findIndex(publication => publication.id === action.payload.pubId);
+      const collaboratorId = action.payload.value as string;
+      const updatedCollaborators = state.list[chosenPubIndex].collaborators.filter(collaborator => collaborator.id !== collaboratorId);
+      state.list[chosenPubIndex].collaborators = updatedCollaborators;
     },
   },
 });
