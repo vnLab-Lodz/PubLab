455320e83ce03d0765603116bc9b3588e6234cac
oski <224107@edu.p.lodz.pl>
2021-11-02 14:08:24 +0100

    added imagepicker component

---
+++ src/renderer/components/ImagePicker/ImagePicker.tsx
@@ -0,0 +1,20 @@
+import React, { ReactElement } from 'react';
+import './ImagePicker.scss';
+
+interface Props { 
+    image?: string;
+    onClick?:  () => void;  
+    error?: boolean;
+}
+
+const ImagePicker: React.FC<Props> = ({ image , error }) => { 
+  
+    if(!image){
+        return error ? (<div className="img error"><div className="imgplus error"/></div>) 
+        : (<div className="img"><div className="imgplus"/></div>) ;  
+    }   else{
+        return <img className="img selected" src={ image }/>;
+    }
+};
+
+export default ImagePicker;
