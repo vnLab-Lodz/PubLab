dab5e18a5d5223f3f48a37bf5cbd73acc23e09a4
Andrzej <kowal.a123@gmail.com>
2021-11-05 02:20:46 +0100

    Placeholder UI for the language picking

--- src/renderer/views/AppSettings/AppSettings.tsx
+++ src/renderer/views/AppSettings/AppSettings.tsx
@@ -1,10 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AppSettings.scss';
+import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
+import { Button, MenuItem, Select } from '@mui/material';
+import {
+  selectCurrentLocale,
+  setLocale,
+} from '../../../shared/redux/slices/settingsSlice';
 
 const AppSettings = () => {
   const { t } = useTranslation();
+  const dispatch = useDispatch();
+  const [language, setLanguage] = useState(useSelector(selectCurrentLocale));
 
-  return <div>{t('AppSettings.title')}</div>;
+  function submitChanges() {
+    dispatch(setLocale(language));
+  }
+
+  return (
+    <div>
+      {t('AppSettings.title')}
+      <Select
+        label={t('AppSettings.language')}
+        value={language}
+        onChange={(e) => setLanguage(e.target.value as 'en' | 'pl')}
+      >
+        <MenuItem value='en'>{t('AppSettings.language.english')}</MenuItem>
+        <MenuItem value='pl'>{t('AppSettings.language.polish')}</MenuItem>
+      </Select>
+      <Button onClick={() => submitChanges()}>{t('common.save')}</Button>
+    </div>
+  );
 };
+
 export default AppSettings;
