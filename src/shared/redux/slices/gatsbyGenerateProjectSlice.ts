b02c7339bb6301634d665a0d86e841e163653acf
Bart1019 <bartlomiej.runowski@gmail.com>
2021-11-06 21:52:35 +0100

    Create gatsby generate project reducer

---
+++ src/shared/redux/slices/gatsbyGenerateProjectSlice.ts
@@ -0,0 +1,48 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { generateProject } from '../../../main/node/gatsby';
+import { createAsyncActionMain } from '../helpers/createActionMain';
+
+type GatsbyGenerateProject = {
+    creating: boolean;
+    creationSuccess: boolean | null;
+  };
+
+const initialState: GatsbyGenerateProject = {
+    creating: false,
+    creationSuccess: null,
+  };
+
+const gatsbyGenerateProjectSlice = createSlice({
+  name: 'projectGenerate',
+  initialState,
+  reducers: {
+    creationExecuting: () => ({
+        creating: true,
+        creationSuccess: null,
+    }),
+    creationFulfilled: () => ({
+        creating: false,
+        creationSuccess: true,
+    }),
+    creationRejected: () => ({
+        creating: false,
+        creationSuccess: false,
+    }),
+  }
+});
+
+export const {
+    creationExecuting,
+    creationFulfilled,
+    creationRejected
+} = gatsbyGenerateProjectSlice.actions
+
+export const generateNewProject = createAsyncActionMain<string[]>(
+    'generateProject',
+    (params) => async (dispatch) => {
+     dispatch(creationExecuting());
+      generateProject(params)
+    }
+)
+
+export default gatsbyGenerateProjectSlice.reducer;
