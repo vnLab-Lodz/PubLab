7dfe914a9d87532bc3e879ea967a58c07c9a6da5
Andrzej <kowal.a123@gmail.com>
2021-11-10 16:09:35 +0100

    feat: :sparkles: check version detail on app startup

--- src/shared/redux/slices/settingsSlice.ts
+++ src/shared/redux/slices/settingsSlice.ts
@@ -1,15 +1,24 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { VersionDetails } from '../../../main/versionDetails';
 import { SupportedLangCode } from '../../../renderer/internationalisation/i18next';
 import { RootState } from '../rootReducer';
 
 type Settings = {
   defaultDirPath: string;
   currentLocale: SupportedLangCode;
+  versionDetails: {
+    version: string;
+    isUpToDate: boolean;
+  };
 };
 
 const initialState: Settings = {
   defaultDirPath: '',
   currentLocale: 'en',
+  versionDetails: {
+    version: '',
+    isUpToDate: false,
+  },
 };
 
 const settingsSlice = createSlice({
@@ -22,15 +31,25 @@ const settingsSlice = createSlice({
     setLocale: (state: Settings, action: PayloadAction<SupportedLangCode>) => {
       state.currentLocale = action.payload;
     },
+    setVersionDetails: (
+      state: Settings,
+      action: PayloadAction<VersionDetails>
+    ) => {
+      state.versionDetails = action.payload;
+    },
   },
 });
 
-export const { setDefaultDirPath, setLocale } = settingsSlice.actions;
+export const { setDefaultDirPath, setLocale, setVersionDetails } =
+  settingsSlice.actions;
 
 export const selectDefaultDirPath = (state: RootState) =>
   state.appSettings.defaultDirPath;
 
 export const selectCurrentLocale = (state: RootState) =>
   state.appSettings.currentLocale;
+
+export const selectVersionDetails = (state: RootState) =>
+  state.appSettings.versionDetails;
 
 export default settingsSlice.reducer;
