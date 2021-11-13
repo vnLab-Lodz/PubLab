1c8869f77e3ed3854acd2cb133ec03c6cd6c8958
Patrycjaaaa <72862573+Patrycjaaaa@users.noreply.github.com>
2021-11-13 15:59:39 +0100

    Add language buttons - finish

--- src/renderer/components/LoginComponent/LoginComponent.tsx
+++ src/renderer/components/LoginComponent/LoginComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  Typography } from '@mui/material';
+import { Typography } from '@mui/material';
 import { FaGithub } from 'react-icons/fa';
 import { useDispatch } from 'react-redux';
 import Button from '@mui/material/Button';
@@ -10,37 +10,45 @@ import logo from '../../assets/publab.png';
 const LoginComponent = () => {
   const dispatch = useDispatch();
   return (
-    
-    <div className = 'background'>
-    <div className='container'>
-   
-   
-    <img src={logo} />
-      <Typography variant='h4' className='text'> 
-      <p>To begin, please log in using your GitHub account </p>
-      </Typography>
-      <div className='button'>
+
+    <div className='background'>
+      <div className='language-box'>
         <Button
           variant='text'
-          className='button'
-          onClick={() => {
-            dispatch(authorizeGitHubUserAsync(false));
-          }}
-        >
-         <FaGithub  className='icon'/>      <p>Log in with GitHub </p>
+          className='language'
+        >     <p>English</p> </Button>
+        <Button
+          variant='text'
+          className='language'
+        >     <p>Polski</p>  </Button>
+      </div>
+      <div className='container'>
+        <img src={logo} />
+        <Typography variant='h4' className='text'>
+          <p>To begin, please log in using your GitHub account </p>
+        </Typography>
+        <div className='button'>
+          <Button
+            variant='text'
+            className='button'
+            onClick={() => {
+              dispatch(authorizeGitHubUserAsync(false));
+            }}
+          >
+            <FaGithub className='icon' />      <p>Log in with GitHub </p>
 
-        </Button>
+          </Button>
 
 
+        </div>
+      </div>
+      <div className='footer'>
+        <Typography variant='h4' >
+          <p> Created by: Marlon Brando, Al Pacino, James Caan, Richard Castellano, Robert Duvall, Sterling Hayden, John Marley, Richard Conte, and Diane Keaton</p>
+        </Typography>
       </div>
     </div>
-<div className='footer'>
-    <Typography variant='h4' > 
-    <p> Created by: Marlon Brando, Al Pacino, James Caan, Richard Castellano, Robert Duvall, Sterling Hayden, John Marley, Richard Conte, and Diane Keaton</p>
-    </Typography>
-    </div>
-    </div>
-    
+
   );
 };
 
