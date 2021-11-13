a20d461d7b3610b8afab0ce98fd745a7107068b6
Pekasiewicz Agnieszka <agnieszka.pekasiewicz@gmail.com>
2021-11-13 15:11:54 +0100

    Removed ESLint errors.

--- src/renderer/components/TextField/TextField.tsx
+++ src/renderer/components/TextField/TextField.tsx
@@ -11,38 +11,50 @@ interface Props {
   onChange?: (...args: any[]) => void;
 }
 
-const StyledTextField = styled(InputBase)(({ type, error, theme: { palette } }) => {
-  const { lightGray, black, lightRed } = palette;
+const iff = (condition: boolean, then: any, otherwise: any) =>
+  condition ? then : otherwise;
 
-  return {
-    color: error === true ? lightRed.main : type === 'light' ? black.main : lightGray.main,
-    border: error === true ? `1px solid ${lightRed.main} `
-      : type === 'light' ? `1px solid ${black.main}` : `1px solid ${lightGray.main}`,
-    height: '45px',
-    width: '400px',
-    paddingTop: '10px',
-    padding: '12px 10px 12px 10px',
-    fontSize: '15px !important'
+const StyledTextField = styled(InputBase)(
+  ({ type, error, theme: { palette } }) => {
+    const { lightGray, black, lightRed } = palette;
+    return {
+      color:
+        error === true
+          ? lightRed.main
+          : iff(type === 'light', black.main, lightGray.main),
+      border:
+        error === true
+          ? `1px solid ${lightRed.main} `
+          : iff(
+              type === 'light',
+              `1px solid ${black.main}`,
+              `1px solid ${lightGray.main}`
+            ),
+      height: '45px',
+      width: '400px',
+      paddingTop: '10px',
+      padding: '12px 10px 12px 10px',
+      fontSize: '15px !important',
+    };
   }
-});
+);
 
-const TextField: React.FC<Props> = (props) => {
-  return (
-    <StyledTextField
-      type={props.type}
-      value={props.value}
-      error={props.error}
-      placeholder={props.placeholder}
-      onChange={() => props.onChange()} />
-  );
-}
+const TextField: React.FC<Props> = (props) => (
+  <StyledTextField
+    type={props.type}
+    value={props.value}
+    error={props.error}
+    placeholder={props.placeholder}
+    onChange={() => props.onChange()}
+  />
+);
 
 TextField.defaultProps = {
   type: 'dark',
   placeholder: 'Project name...',
   value: undefined,
   error: false,
-  onChange: () => { }
+  onChange: () => {},
 };
 
 export default TextField;
