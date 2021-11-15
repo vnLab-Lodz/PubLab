8da58bfd73f7f70486fb48dada11c92c2f3892af
224088 <224088@edu.p.lodz.pl>
2021-11-16 00:59:15 +0100

    trying to implement language change

--- src/renderer/components/LoginComponent/LoginComponent.tsx
+++ src/renderer/components/LoginComponent/LoginComponent.tsx
@@ -1,30 +1,53 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import { Typography } from '@mui/material';
 import { FaGithub } from 'react-icons/fa';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
 import { authorizeGitHubUserAsync } from '../../../shared/redux/slices/currentUserSlice';
 import './LoginComponent.scss';
 import logo from '../../assets/publab.png';
+import { useTranslation } from 'react-i18next';
+import { supportedLocales } from '../../internationalisation/i18next';
+import {
+  selectAllSettings,
+  setAllSettings,
+  Settings,
+} from '../../../shared/redux/slices/settingsSlice';
 
 const LoginComponent = () => {
+  const { t } = useTranslation();
   const dispatch = useDispatch();
+  const [settings, changeSetting] = useReducer(
+    handleSettingChange,
+    useSelector(selectAllSettings)
+  );
+
+  function submitChanges() {
+    dispatch(setAllSettings(settings));
+  }
+  function changeLanguage(x: string) {
+    if (x === "EN")
+      changeSetting({ currentLocale: "pl" });
+    else
+      changeSetting({ currentLocale: "pl" });
+    console.log(settings.currentLocale);
+    submitChanges();
+  }
   return (
     <div className='background'>
+
       <div className='language-box'>
-        <Button variant='text' className='language'>
-          {' '}
-          <p>English</p>{' '}
-        </Button>
-        <Button variant='text' className='language'>
-          {' '}
-          <p>Polski</p>{' '}
-        </Button>
+        {supportedLocales.map((locale) => (
+          <Button key={locale} variant='text' className='language' onClick={() => changeLanguage("EN")}>
+            <Typography>{(`${locale}`)}</Typography>
+          </Button>
+        ))}
       </div>
+
       <div className='container'>
         <img src={logo} alt='Logo' />
-        <Typography variant='h4' className='text'>
-          <p>To begin, please log in using your GitHub account </p>
+        <Typography variant='h4' className='text' onClick={() => changeLanguage("PL")}>
+          <p>{t('login-screen.message')} </p>
         </Typography>
         <div className='button'>
           <Button
@@ -34,7 +57,7 @@ const LoginComponent = () => {
               dispatch(authorizeGitHubUserAsync(false));
             }}
           >
-            <FaGithub className='icon' /> <p>Log in with GitHub </p>
+            <FaGithub className='icon' /> <p>{t('login-screen.button')} </p>
           </Button>
         </div>
       </div>
@@ -42,7 +65,7 @@ const LoginComponent = () => {
         <Typography variant='h4'>
           <p>
             {' '}
-            Created by: Marlon Brando, Al Pacino, James Caan, Richard
+            {t('login-screen.created')} Marlon Brando, Al Pacino, James Caan, Richard
             Castellano, Robert Duvall, Sterling Hayden, John Marley, Richard
             Conte, and Diane Keaton
           </p>
@@ -53,3 +76,10 @@ const LoginComponent = () => {
 };
 
 export default LoginComponent;
+
+function handleSettingChange(
+  state: Settings,
+  change: Partial<Settings>
+): Settings {
+  return { ...state, ...change };
+}
