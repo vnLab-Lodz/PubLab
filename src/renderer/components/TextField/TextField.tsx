8b299ad8a2729119775513f38361fb64e9d155db
Pekasiewicz Agnieszka <agnieszka.pekasiewicz@gmail.com>
2021-11-15 17:22:08 +0100

    Code simplification after recommendations from pull request.

--- src/renderer/components/TextField/TextField.tsx
+++ src/renderer/components/TextField/TextField.tsx
@@ -1,60 +1,21 @@
-import * as React from 'react';
 import { InputBase } from '@mui/material';
 import './TextField.scss';
 import { styled } from '@mui/material/styles';
 
-interface Props {
-  type?: string;
-  placeholder?: string;
-  error?: boolean;
-  value?: string;
-  onChange?: (...args: any[]) => void;
-}
+export default styled(InputBase)(({ type, error, theme: { palette } }) => {
+  const { lightGray, black, lightRed } = palette;
 
-const iff = (condition: boolean, then: any, otherwise: any) =>
-  condition ? then : otherwise;
+  let color = type === 'light' ? black.main : lightGray.main;
+  if (error) color = lightRed.main;
 
-const StyledTextField = styled(InputBase)(
-  ({ type, error, theme: { palette } }) => {
-    const { lightGray, black, lightRed } = palette;
-    return {
-      color:
-        error === true
-          ? lightRed.main
-          : iff(type === 'light', black.main, lightGray.main),
-      border:
-        error === true
-          ? `1px solid ${lightRed.main} `
-          : iff(
-              type === 'light',
-              `1px solid ${black.main}`,
-              `1px solid ${lightGray.main}`
-            ),
-      height: '45px',
-      width: '400px',
-      paddingTop: '10px',
-      padding: '12px 10px 12px 10px',
-      fontSize: '15px !important',
-    };
-  }
-);
+  const border = `1px solid ${color}`;
 
-const TextField: React.FC<Props> = (props) => (
-  <StyledTextField
-    type={props.type}
-    value={props.value}
-    error={props.error}
-    placeholder={props.placeholder}
-    onChange={() => props.onChange()}
-  />
-);
-
-TextField.defaultProps = {
-  type: 'dark',
-  placeholder: 'Project name...',
-  value: undefined,
-  error: false,
-  onChange: () => {},
-};
-
-export default TextField;
+  return {
+    color,
+    border,
+    height: '45px',
+    width: '400px',
+    padding: '12px 10px 12px 10px',
+    fontSize: '15px !important',
+  };
+});
