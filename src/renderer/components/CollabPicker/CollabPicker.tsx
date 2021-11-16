f37f1c45353de50f40d60e147c835cd245e882a7
przemko2109 <72570536+przemko2109@users.noreply.github.com>
2021-11-16 13:01:01 +0100

    Second change in Collaboration Picker

--- src/renderer/components/CollabPicker/CollabPicker.tsx
+++ src/renderer/components/CollabPicker/CollabPicker.tsx
@@ -1,52 +1,73 @@
 import * as React from 'react';
-import './CollabPicker.scss';
-import TextField from '../TextField/TextField';
-import Button from '../Button/Button';
-import Select from '@mui/material/Select';
-import { MenuItem } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { Button, MenuItem, TextField } from '@mui/material';
 import { useState } from 'react';
+import { useEffect } from '@storybook/addons';
+import { ChangeEvent } from 'hoist-non-react-statics/node_modules/@types/react';
 
 interface Value {
-  username: string;
-  role: number;
+    username: string;
+    role: string;
 }
 
-interface Options {
-  value: Value;
-  label: string;
+interface Option {
+    value: string;
+    label: string;
 }
 
 interface Props {
-  value?: Value;
-  options?: Options;
-  onChange?: (...args: any[]) => void;
+    value?: Value;
+    options?: Option[];
+    onChange?: (value: Value | undefined) => void;
+    onAdd?: (value: Value) => void;
 }
 
-const CollabPicker: React.FC<Props> = ({ options, onChange }) => {
-  [username, onChange] = useState('');
-  const array = [{ role: value?.ro, label: string }];
-  return (
-    <div>
-      <TextField
-        placeholder='Username...'
-        value={name}
-        error={false}
-        onChange={onChange}
-      />
-      <Select label='Role'>
-        {array.map(({ role, label }) => (
-          <MenuItem value={role}>{label}</MenuItem>
-        ))}
-      </Select>
-      <Button>ADD</Button>
-    </div>
-  );
+const CollabPicker: React.FC<Props> = ({value, options, onChange, onAdd}) => {
+    const [currentValue, setCurrentValue] = useState(value)
+
+    useEffect(() => {
+        if (onChange === undefined) return;
+
+        onChange(currentValue);
+    }, [currentValue]);
+
+    const handleUsernameChange = (event: ChangeEvent<HTMLInputElement>) => 
+        setCurrentValue((prev) => {
+            const username = event.target.value;
+            return {username, role: prev?.role ?? ''}
+        });
+
+    const handleRoleChange = (event: ChangeEvent<HTMLInputElement>) =>
+        setCurrentValue((prev) => {
+            const role = event.target.value;
+            return {role, username: prev?.username ?? ''}
+        });
+
+    const handleAdd = () => {
+        if (onAdd === undefined || currentValue === undefined) return;
+
+        onAdd(currentValue);
+        setCurrentValue(undefined);
+    };
+
+    const currentUsername = currentValue?.username ?? '';
+    const currentRole = currentValue?.role ?? '';
+
+    return <div>
+        <TextField placeholder='Username...' value={currentUsername} onChange={handleUsernameChange}/>
+        <Select label="Role" value={currentRole} onChange={handleRoleChange}>
+            {
+                options?.map(({value, label}) => <MenuItem value={value} key={value}>{label}</MenuItem>)
+            }
+        </Select>
+        <Button onClick={handleAdd}>ADD</Button>
+    </div>;
 };
 
 CollabPicker.defaultProps = {
-  value: undefined,
-  options: undefined,
-  onChange: () => {},
+    value: undefined,
+    options: undefined,
+    onChange: () => {},
 };
 
 export default CollabPicker;
