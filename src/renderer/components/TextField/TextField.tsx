99a6d78b8fb20b786b305592e762cdd34d2f57f5
agapekasiewicz <92737244+agapekasiewicz@users.noreply.github.com>
2021-11-16 14:20:13 +0100

    Text field component (#113)
    * Basic text field added
    
    * Initial styling added
    
    * Style for TextField added
    
    * Styling with colors from palette added
    
    * Corrected font size. Small fixes in files.
    
    * Removed redundant argument.
    
    * Prettified Code!
    
    * Removed ESLint errors.
    
    * Code simplification after recommendations from pull request.
    
    Co-authored-by: agapekasiewicz <agapekasiewicz@users.noreply.github.com>

---
+++ src/renderer/components/TextField/TextField.tsx
@@ -0,0 +1,21 @@
+import { InputBase } from '@mui/material';
+import './TextField.scss';
+import { styled } from '@mui/material/styles';
+
+export default styled(InputBase)(({ type, error, theme: { palette } }) => {
+  const { lightGray, black, lightRed } = palette;
+
+  let color = type === 'light' ? black.main : lightGray.main;
+  if (error) color = lightRed.main;
+
+  const border = `1px solid ${color}`;
+
+  return {
+    color,
+    border,
+    height: '45px',
+    width: '400px',
+    padding: '12px 10px 12px 10px',
+    fontSize: '15px !important',
+  };
+});
