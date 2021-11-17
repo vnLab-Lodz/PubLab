c56002f5ad5ad6ddb29c5866cacc220f930f0375
Piotr Karczewski <p.karczewsk@gmail.com>
2021-11-17 13:23:04 +0000

    refactor: :recycle: Refactor radio btn

--- src/renderer/components/RadioButton/RadioGroup.tsx
+++ src/renderer/components/RadioButton/RadioGroup.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import { styled } from '@mui/material/styles';
-import Radio, { RadioProps } from '@mui/material/Radio';
+import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
+// import FormLabel from '@mui/material/FormLabel';
 import { Typography } from '@mui/material';
-import { Box } from '@mui/system';
+// import { Box } from '@mui/';
+import { theme } from '../../theme';
 
 interface Props {
   disabled1?: boolean;
@@ -14,73 +15,28 @@ interface Props {
   defaults?: string;
 }
 
-const BpIcon = styled('span')(({ theme }) => ({
-  borderRadius: '50%',
-  width: 20,
-  height: 20,
-  boxShadow:
-    theme.palette.mode === 'dark'
-      ? '0 0 0 1px #DDDDDD'
-      : 'inset 0 0 0 3.5px rgb(0,0,0), inset 0 -3.5px 0 #DDDDDD',
-  border: '1px solid #DDDDDD',
-  backgroundColor: theme.palette.mode === 'dark' ? '#000' : '#000',
-  '.Mui-focusVisible &': {
-    outline: '1px auto #DDDDDD',
-    outlineOffset: 1,
-  },
-  'input:hover ~ &': {
-    backgroundColor: theme.palette.mode === 'dark' ? '#30404d' : '#ebf1f5',
-  },
-  'input:disabled ~ &': {
-    boxShadow: 'none',
-    background:
-      theme.palette.mode === 'dark'
-        ? 'rgba(57,75,89,.5)'
-        : 'rgba(206,217,224,.5)',
+const RadioBtn = styled(Radio)(() => ({
+  color: theme.palette.lightGray.main,
+  '&.Mui-checked': {
+    color: theme.palette.lightGray.main,
   },
 }));
 
-const BpCheckedIcon = styled(BpIcon)({
-  backgroundColor: '#fff',
-
-  'input:hover ~ &': {
-    backgroundColor: '#DDDDDD',
-  },
-});
-
-// Inspired by blueprintjs
-function BpRadio(props: RadioProps) {
-  return (
-    <Radio
-      sx={{
-        '&:hover': {
-          bgcolor: 'black',
-        },
-      }}
-      disableRipple
-      color='default'
-      checkedIcon={<BpCheckedIcon />}
-      icon={<BpIcon />}
-      {...props}
-    />
-  );
-}
-
 const OurRadio: React.FC<Props> = ({ disabled1, disabled2, defaults }) => (
   <FormControl component='fieldset'>
-    <Box pb={4} pt={2}>
+    {/* <Box pb={4} pt={2}>
       <FormLabel component='legend'>
         <Typography variant='h4' color='text.primary'>
           {' '}
           Choose the Package Manager:
         </Typography>
       </FormLabel>
-    </Box>
+    </Box> */}
     <RadioGroup defaultValue={defaults} name='customized-radios'>
       <FormControlLabel
         disabled={disabled1}
         value='yarn'
-        control={<BpRadio />}
+        control={<RadioBtn />}
         label={
           <Typography variant='h5' color='default'>
             YARN
@@ -90,7 +46,7 @@ const OurRadio: React.FC<Props> = ({ disabled1, disabled2, defaults }) => (
       <FormControlLabel
         disabled={disabled2}
         value='npm'
-        control={<BpRadio />}
+        control={<RadioBtn s />}
         label={
           <Typography variant='h5' color='default'>
             NPM
