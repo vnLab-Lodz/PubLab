0106fbc834a0d2b9b596c4df57bd72a2e77ba6c2
224088 <224088@edu.p.lodz.pl>
2021-11-17 20:02:51 +0100

    ESLint fight fight fight

--- src/renderer/components/LoginComponent/LoginComponent.tsx
+++ src/renderer/components/LoginComponent/LoginComponent.tsx
@@ -3,11 +3,11 @@ import { Typography } from '@mui/material';
 import { FaGithub } from 'react-icons/fa';
 import { useDispatch, useSelector } from 'react-redux';
 import Button from '@mui/material/Button';
+import { useTranslation } from 'react-i18next';
+import { supportedLocales } from '../../internationalisation/i18next';
 import { authorizeGitHubUserAsync } from '../../../shared/redux/slices/currentUserSlice';
 import './LoginComponent.scss';
 import logo from '../../assets/publab.png';
-import { useTranslation } from 'react-i18next';
-import { supportedLocales } from '../../internationalisation/i18next';
 import {
   selectAllSettings,
   setAllSettings,
@@ -26,27 +26,25 @@ const LoginComponent = () => {
     dispatch(setAllSettings(settings));
   }
   function changeLanguage(x: string) {
-    if (x === "EN")
-      changeSetting({ currentLocale: "pl" });
+    if (x === 'EN')
+      changeSetting({ currentLocale: 'pl' });
     else
-      changeSetting({ currentLocale: "pl" });
-    console.log(settings.currentLocale);
+      changeSetting({ currentLocale: 'pl' });
     submitChanges();
   }
   return (
     <div className='background'>
-
       <div className='language-box'>
         {supportedLocales.map((locale) => (
-          <Button key={locale} variant='text' className='language' onClick={() => changeLanguage("EN")}>
-            <Typography>{(`${locale}`)}</Typography>
+          <Button key={locale} variant='text' className='language' onClick={() => changeLanguage('EN')}>
+            <Typography>{"${locale}"}</Typography>
           </Button>
         ))}
       </div>
 
       <div className='container'>
         <img src={logo} alt='Logo' />
-        <Typography variant='h4' className='text' onClick={() => changeLanguage("PL")}>
+        <Typography variant='h4' className='text' onClick={() => changeLanguage('PL')}>
           <p>{t('login-screen.message')} </p>
         </Typography>
         <div className='button'>
@@ -65,8 +63,9 @@ const LoginComponent = () => {
         <Typography variant='h4'>
           <p>
             {' '}
-            {t('login-screen.created')} Marlon Brando, Al Pacino, James Caan, Richard
-            Castellano, Robert Duvall, Sterling Hayden, John Marley, Richard Conte, and Diane Keaton
+            {t('login-screen.created')} Marlon Brando, Al Pacino, James Caan,
+            Richard Castellano, Robert Duvall, Sterling Hayden, John Marley,
+            Richard Conte, and Diane Keaton
           </p>
         </Typography>
       </div>
