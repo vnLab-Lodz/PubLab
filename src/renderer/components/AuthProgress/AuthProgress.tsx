6e5e0d1f2f05d047137a5c40ff85fbbc859794f3
Piotr Karczewski <p.karczewsk@gmail.com>
2021-11-26 08:59:00 +0100

    feat: :globe_with_meridians: Internationalize AuthProgress component

--- src/renderer/components/AuthProgress/AuthProgress.tsx
+++ src/renderer/components/AuthProgress/AuthProgress.tsx
@@ -1,30 +1,19 @@
 import React from 'react';
 import { Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 import { AUTH_STATES } from '../../../shared/redux/slices/currentUserSlice';
 import * as Styled from './style';
 import Logo from '../../assets/publab.png';
 
 const AuthProgress: React.FC<{ status: AUTH_STATES }> = ({ status }) => {
-  const getMessage = () => {
-    switch (status) {
-      case AUTH_STATES.PRE_AUTHORIZE:
-        return 'Attempting log in...';
-      case AUTH_STATES.CODE_REQUESTED:
-        return 'Requesting access token...';
-      case AUTH_STATES.TOKEN_REQUESTED:
-        return 'Fetching user data...';
-      case AUTH_STATES.AUTH_FAILED:
-        return 'Failed to log in. Please try again.';
-      default:
-        return 'Logged in.';
-    }
-  };
+  const { t } = useTranslation();
+  const key = status.toLowerCase() as Lowercase<AUTH_STATES>;
 
   return (
     <Styled.Container>
       <Styled.LogoImg src={Logo} alt='PubLab' />
       <Typography variant='h5'>
-        Authentication in progress: {getMessage()}
+        {t('auth.in_progress')} {t(`auth.states.${key}`)}
       </Typography>
     </Styled.Container>
   );
