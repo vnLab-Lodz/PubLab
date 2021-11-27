3ae2e26b442d77d9438cbf6b7c566b6a808adb3d
lprakapovich <lprakapovich@users.noreply.github.com>
2021-11-27 22:58:10 +0000

    Prettified Code!

--- src/shared/redux/slices/nodeCheckSlice.test.ts
+++ src/shared/redux/slices/nodeCheckSlice.test.ts
@@ -1,68 +1,72 @@
-import reducer, {checkingInstallation, checkNode, nodeInstalled, nodeNotInstalled} from './nodeCheckSlice';
+import reducer, {
+  checkingInstallation,
+  checkNode,
+  nodeInstalled,
+  nodeNotInstalled,
+} from './nodeCheckSlice';
 
 const initState = {
-    installed: null,
-    checkingInstall: true
-}
+  installed: null,
+  checkingInstall: true,
+};
 
 jest.mock('../../../main/node/nodeCheck', () => {
-    const originalModule = jest.requireActual('../../../main/node/nodeCheck');
-    return {
-        __esModule: true,
-        ...originalModule,
-        checkForNode: jest.fn()
-            .mockResolvedValueOnce(false)
-            .mockResolvedValue(true)
-    };
+  const originalModule = jest.requireActual('../../../main/node/nodeCheck');
+  return {
+    __esModule: true,
+    ...originalModule,
+    checkForNode: jest
+      .fn()
+      .mockResolvedValueOnce(false)
+      .mockResolvedValue(true),
+  };
 });
 
 describe('nodeCheckSlice', () => {
-    it('dispatches nodeNotInstalled action if node is not globally installed',  () => {
-         checkNode();
-         // checkForNode() will resolve to false here
-         // how can I check if nodeNotInstalled was dispatched?
+  it('dispatches nodeNotInstalled action if node is not globally installed', () => {
+    checkNode();
+    // checkForNode() will resolve to false here
+    // how can I check if nodeNotInstalled was dispatched?
+  });
+  it('dispatches nodeInstalled action if node is truly globally installed', () => {
+    checkNode();
+    // checkForNode() will resolve to true here
+    // how can I check if nodeInstalled was dispatched?
+  });
+  it('handles checkingInstallation action', () => {
+    expect(reducer(undefined, checkingInstallation())).toEqual({
+      installed: null,
+      checkingInstall: true,
     });
-    it('dispatches nodeInstalled action if node is truly globally installed',  () => {
-        checkNode();
-        // checkForNode() will resolve to true here
-        // how can I check if nodeInstalled was dispatched?
+  });
+  it('handles nodeInstalled action', () => {
+    expect(reducer(undefined, nodeInstalled())).toEqual({
+      installed: true,
+      checkingInstall: false,
     });
-    it('handles checkingInstallation action', () => {
-        expect(reducer(undefined, checkingInstallation())).toEqual({
-            installed: null,
-            checkingInstall: true,
-        });
+  });
+  it('handles nodeInstalled action', () => {
+    expect(reducer(undefined, nodeNotInstalled())).toEqual({
+      installed: false,
+      checkingInstall: false,
     });
-    it('handles nodeInstalled action', () => {
-        expect(reducer(undefined, nodeInstalled())).toEqual({
-            installed: true,
-            checkingInstall: false
-        })
-    })
-    it('handles nodeInstalled action', () => {
-        expect(reducer(undefined, nodeNotInstalled())).toEqual({
-            installed: false,
-            checkingInstall: false
-        })
-    })
-    it('updates current state properly', () => {
-        expect(reducer(initState, checkingInstallation())).toEqual({
-            installed: null,
-            checkingInstall: true,
-        });
+  });
+  it('updates current state properly', () => {
+    expect(reducer(initState, checkingInstallation())).toEqual({
+      installed: null,
+      checkingInstall: true,
     });
-    it('updates current state properly', () => {
-        expect(reducer(initState, nodeInstalled())).toEqual({
-            installed: true,
-            checkingInstall: false
-        })
-    })
-    it('updates current state properly', () => {
-        expect(reducer(initState, nodeNotInstalled())).toEqual({
-            installed: false,
-            checkingInstall: false
-        })
-    })
+  });
+  it('updates current state properly', () => {
+    expect(reducer(initState, nodeInstalled())).toEqual({
+      installed: true,
+      checkingInstall: false,
+    });
+  });
+  it('updates current state properly', () => {
+    expect(reducer(initState, nodeNotInstalled())).toEqual({
+      installed: false,
+      checkingInstall: false,
+    });
+  });
 });
-
-
