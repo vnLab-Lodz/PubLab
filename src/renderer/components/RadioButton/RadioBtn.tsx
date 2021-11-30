f233538d206aa1744365fe87aebf2c555d9f1af5
LostInStatic <kowal.a1234@gmail.com>
2021-11-30 04:02:35 +0100

    refactor: :lipstick: change radio and switch components to make use of themes

--- src/renderer/components/RadioButton/RadioBtn.tsx
+++ src/renderer/components/RadioButton/RadioBtn.tsx
@@ -2,9 +2,9 @@ import { styled } from '@mui/material/styles';
 import Radio from '@mui/material/Radio';
 
 const RadioBtn = styled(Radio)(({ theme }) => ({
-  color: theme.palette.lightGray.main,
+  color: theme.palette.primary.main,
   '&.Mui-checked': {
-    color: theme.palette.lightGray.main,
+    color: theme.palette.primary.main,
   },
   '&.Mui-disabled': {
     color: theme.palette.darkGray.main,
