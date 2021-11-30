3f05524fbd8d191528eca4eb718a3c6d70af754f
Bart1019 <bartlomiej.runowski@gmail.com>
2021-11-30 19:58:30 +0100

    Lint fixes

--- src/shared/redux/slices/settingsSlice.test.ts
+++ src/shared/redux/slices/settingsSlice.test.ts
@@ -1,17 +1,20 @@
-import reducer, { NOTIFICATION_INTERVAL, setSettings, Settings } from "./settingsSlice";
-
+import reducer, {
+  NOTIFICATION_INTERVAL,
+  setSettings,
+  Settings,
+} from './settingsSlice';
 
 describe('settings', () => {
-    it('handles setSettings action', () => {
-        const settings: Settings = {
-            defaultDirPath: 'default_path',
-            currentLocale: 'en',
-            notificationInterval: NOTIFICATION_INTERVAL.INSTANT
-        };
-        expect(reducer(undefined, setSettings(settings))).toEqual({
-            defaultDirPath: 'default_path',
-            currentLocale: 'en',
-            notificationInterval: NOTIFICATION_INTERVAL.INSTANT
-      })
+  it('handles setSettings action', () => {
+    const settings: Settings = {
+      defaultDirPath: 'default_path',
+      currentLocale: 'en',
+      notificationInterval: NOTIFICATION_INTERVAL.INSTANT,
+    };
+    expect(reducer(undefined, setSettings(settings))).toEqual({
+      defaultDirPath: 'default_path',
+      currentLocale: 'en',
+      notificationInterval: NOTIFICATION_INTERVAL.INSTANT,
     });
+  });
 });
