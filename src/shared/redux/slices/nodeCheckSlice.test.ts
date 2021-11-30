520e32539a05ef0f5bac6e2fb9ba020aeefc1574
Piotr Karczewski <p.karczewsk@gmail.com>
2021-11-30 19:41:16 +0100

    test: :fire: Remove redundant tests

--- src/shared/redux/slices/nodeCheckSlice.test.ts
+++ src/shared/redux/slices/nodeCheckSlice.test.ts
@@ -4,11 +4,6 @@ import reducer, {
   nodeNotInstalled,
 } from './nodeCheckSlice';
 
-const initState = {
-  installed: null,
-  checkingInstall: true,
-};
-
 describe('nodeCheckSlice', () => {
   it('handles checkingInstallation action', () => {
     expect(reducer(undefined, checkingInstallation())).toEqual({
@@ -16,32 +11,16 @@ describe('nodeCheckSlice', () => {
       checkingInstall: true,
     });
   });
+
   it('handles nodeInstalled action', () => {
     expect(reducer(undefined, nodeInstalled())).toEqual({
       installed: true,
       checkingInstall: false,
     });
   });
+
   it('handles nodeInstalled action', () => {
     expect(reducer(undefined, nodeNotInstalled())).toEqual({
-      installed: false,
-      checkingInstall: false,
-    });
-  });
-  it('updates current state properly', () => {
-    expect(reducer(initState, checkingInstallation())).toEqual({
-      installed: null,
-      checkingInstall: true,
-    });
-  });
-  it('updates current state properly', () => {
-    expect(reducer(initState, nodeInstalled())).toEqual({
-      installed: true,
-      checkingInstall: false,
-    });
-  });
-  it('updates current state properly', () => {
-    expect(reducer(initState, nodeNotInstalled())).toEqual({
       installed: false,
       checkingInstall: false,
     });
