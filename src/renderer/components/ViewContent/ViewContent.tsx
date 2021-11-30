cfdb0fd58a1e66a3e5c4b6fdc1c33e657c43c9ed
LostInStatic <kowal.a1234@gmail.com>
2021-11-30 12:26:43 +0100

    refactor: :lipstick: improve handling of  ViewContent children

--- src/renderer/components/ViewContent/ViewContent.tsx
+++ src/renderer/components/ViewContent/ViewContent.tsx
@@ -2,9 +2,9 @@ import { BoxProps } from '@mui/material';
 import React from 'react';
 import { BackgroundWrapper, ContentBox } from './style';
 
-const ViewContent: React.FC<BoxProps> = (props) => (
-  <BackgroundWrapper {...props}>
-    <ContentBox>{props.children}</ContentBox>
+const ViewContent: React.FC<BoxProps> = ({ children, ...rest }) => (
+  <BackgroundWrapper {...rest}>
+    <ContentBox>{children}</ContentBox>
   </BackgroundWrapper>
 );
 
