c3a1d79a5d757aa15a1edfe1fa0bd373f736c98c
JKusiak <JKusiak@users.noreply.github.com>
2021-12-02 20:40:43 +0000

    Prettified Code!

--- src/renderer/views/AddProject/AddProject.tsx
+++ src/renderer/views/AddProject/AddProject.tsx
@@ -1,29 +1,27 @@
-import React from "react";
+import React from 'react';
 import { styled } from '@mui/system';
-import { useStore } from "react-redux";
-import { increaseStep } from "./../../../shared/redux/slices/addPublicationSlice";
-import { TextField } from "@mui/material";
-import { useEffect } from "hoist-non-react-statics/node_modules/@types/react";
+import { useStore } from 'react-redux';
+import { increaseStep } from './../../../shared/redux/slices/addPublicationSlice';
+import { TextField } from '@mui/material';
+import { useEffect } from 'hoist-non-react-statics/node_modules/@types/react';
 
 const StyledContainer = styled('div')(({ theme }) => ({
-	width: '100%',
-	height: '100%',
-	background: theme.palette.black.main,
+  width: '100%',
+  height: '100%',
+  background: theme.palette.black.main,
 }));
 
 const AddProject = () => {
   const store = useStore();
   const storeState = store.getState();
 
-  useEffect(() => {
-    
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <StyledContainer>
-	  <TextField>New Project</TextField>
-	  <TextField>step {storeState.newPublication.step}/4</TextField>
-	  <button onClick={() => store.dispatch(increaseStep())}>next</button>
+      <TextField>New Project</TextField>
+      <TextField>step {storeState.newPublication.step}/4</TextField>
+      <button onClick={() => store.dispatch(increaseStep())}>next</button>
     </StyledContainer>
   );
 };
