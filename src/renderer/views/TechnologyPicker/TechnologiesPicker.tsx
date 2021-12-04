9a20ffa6f55a73adb66464fbfbce040dacb9fc7a
Kuba <kubajaru@gmail.com>
2021-12-04 17:26:20 +0100

    Add styling

--- src/renderer/views/TechnologyPicker/TechnologiesPicker.tsx
+++ src/renderer/views/TechnologyPicker/TechnologiesPicker.tsx
@@ -2,16 +2,23 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Button from '../../components/Button/Button';
 import StyledSwitch from '../../components/Switch/Switch';
+import './TechnologiesPicker.scss';
 
 const TechnologiesPicker = () => {
     const { t, i18n } = useTranslation();
 
-    return (<div>
-        <p>{t('technology-picker.message')}</p>
-        <div><StyledSwitch checked={true}/> <p>SASS</p></div>
-        <div><StyledSwitch checked={true}/><p>TYPESCRIPT</p></div>
+    return (<div className='container'>
+        <h2>{t('technology-picker.message')}</h2>
+        
+        <div className='switches'>
+        <div className='switch'><StyledSwitch/><p className='inline'> SCSS</p></div>
+        <div><StyledSwitch/><p className='inline'> TYPESCRIPT</p ></div>
+        </div>
+
+        <div>
         <Button>{t('technology-picker.back-button')}</Button>
         <Button>{t('technology-picker.next-button')}</Button>
+        </div>
     </div>);
 }
 
