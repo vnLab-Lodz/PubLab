2a154cc37d09635034ed570b96e486179becc86e
Juliusz <juliusz.kusiak@gmail.com>
2021-12-06 22:02:33 +0100

    Fix styling of the component, add internationalization

--- src/renderer/views/AddProject/AddProject.tsx
+++ src/renderer/views/AddProject/AddProject.tsx
@@ -1,67 +1,24 @@
 import React from 'react';
 import { styled } from '@mui/system';
-import { Typography } from '@mui/material';
+import { ThemeProvider, Typography } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
 import {
   decreaseStep,
   increaseStep,
 } from '../../../shared/redux/slices/addPublicationSlice';
 import { RootState } from '../../../shared/redux/rootReducer';
+import { altTheme } from './../../theme';
+import ViewContent from '../../../renderer/components/ViewContent/ViewContent';
+import * as Styled from './style';
+import { useTranslation } from 'react-i18next';
 
-const StyledBackground = styled('div')(({ theme }) => ({
-  width: '100%',
-  height: '100%',
-  background: theme.palette.background,
-}));
-
-const StyledContainer = styled('div')(({ theme }) => ({
-  marginLeft: 'auto',
-  marginRight: 'auto',
-  display: 'flex',
-  flexDirection: 'column',
-  width: '50%',
-  background: theme.palette.background,
-  paddingTop: theme.spacing(2),
-}));
-
-const StepContainer = styled('div')(({ theme }) => ({
-  marginTop: theme.spacing(10),
-  marginBottom: theme.spacing(8),
-}));
-
-const StyledButtonBlack = styled('button')(({ theme }) => ({
-  width: '50%',
-  height: '60px',
-  border: '1px solid',
-  borderColor: theme.palette.text.primary,
-  outline: 'none',
-  color: theme.palette.text.primary,
-  backgroundColor: 'transparent',
-  fontSize: '15px',
-  fontWeight: 'bold',
-  '&:hover': {
-    cursor: 'pointer',
-  },
-}));
-
-const StyledButtonGreen = styled('button')(({ theme }) => ({
-  width: '50%',
-  height: '60px',
-  border: 'none',
-  outline: 'none',
-  background: theme.palette.green.main,
-  fontSize: '15px',
-  fontWeight: 'bold',
-  '&:hover': {
-    cursor: 'pointer',
-  },
-}));
 
 const AddProject = () => {
   const currentStep = useSelector(
     (state: RootState) => state.newPublication.step
   );
   const dispatch = useDispatch();
+  const { t } = useTranslation();
 
   const renderStepComponent = () => {
     switch (currentStep) {
@@ -83,31 +40,52 @@ const AddProject = () => {
   };
 
   return (
-    <StyledBackground>
-      <StyledContainer>
-        <Typography variant='h1' style={{ fontWeight: 'bold' }}>
-          New Project
+    <ThemeProvider theme={altTheme}>
+      <ViewContent>
+        <Typography 
+          variant='h1'
+          sx={{
+            color: (theme) => theme.palette.text.primary,
+            fontWeight: 'bold'
+          }}
+        >
+          {t('AddProject.header.newProject')}
         </Typography>
-        <Typography variant='h4'>step {currentStep}/5</Typography>
-        <StepContainer>{renderStepComponent()}</StepContainer>
+        <Typography 
+          variant='h4'
+          sx={{
+            color: (theme) => theme.palette.text.primary,
+          }}
+        >
+           {t('AddProject.header.step')} {currentStep}/5
+        </Typography>
+        <Styled.StepContainer>{renderStepComponent()}</Styled.StepContainer>
         <div>
-          <StyledButtonBlack
+          <Styled.BlackButton
+            textCase='uppercase'
+            fontWeight='bold'
+            typographyVariant='h3'
+            variant='outlined'
             onClick={() => {
               if (currentStep > 1) dispatch(decreaseStep());
             }}
           >
-            BACK
-          </StyledButtonBlack>
-          <StyledButtonGreen
+            {t('AddProject.buttons.back')}
+          </Styled.BlackButton>
+          <Styled.GreenButton
+            textCase='uppercase'
+            fontWeight='bold'
+            typographyVariant='h3'
+            color='green'
             onClick={() => {
               if (currentStep < 5) dispatch(increaseStep());
             }}
           >
-            NEXT
-          </StyledButtonGreen>
+            {t('AddProject.buttons.next')}
+          </Styled.GreenButton>
         </div>
-      </StyledContainer>
-    </StyledBackground>
+      </ViewContent>
+    </ThemeProvider>
   );
 };
 
---
+++ src/renderer/views/AddProject/style.ts
@@ -0,0 +1,23 @@
+import { styled } from '@mui/material';
+import Button from '../../../renderer/components/Button/Button';
+
+export const StepContainer = styled('div')(({ theme }) => ({
+  marginTop: theme.spacing(10),
+  marginBottom: theme.spacing(8),
+}));
+
+export const StyledButton = styled(Button)(() => ({
+  width: '50%',
+  height: '60px',
+}));
+
+export const BlackButton = styled(StyledButton)(({ theme }) => ({
+  backgroundColor: theme.palette.black,
+}));
+
+  
+export const GreenButton = styled(StyledButton)(({ theme }) => ({
+	backgroundColor: theme.palette.green.main,
+	color: theme.palette.black.main,
+}));
+  
