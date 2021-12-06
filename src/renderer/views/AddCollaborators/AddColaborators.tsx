b14d0cc0789593110151142166548cff1c8bd43d
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-12-06 21:07:18 +0100

    Add title translation

--- src/renderer/views/AddCollaborators/AddColaborators.tsx
+++ src/renderer/views/AddCollaborators/AddColaborators.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { ThemeProvider, Typography } from '@mui/material';
 import { altTheme } from '../../theme';
 import ViewContentBase from '../../components/ViewContent/ViewContent';
 
-const AddColaborators = () => (
-  <ThemeProvider theme={altTheme}>
-    <ViewContentBase>
-      <Typography
-        variant='h1'
-        sx={{
-          color: (theme) => theme.palette.text.primary,
-          marginBottom: '8.5rem',
-        }}
-      >
-        Would you like to add colaborators?
-      </Typography>
-    </ViewContentBase>
-  </ThemeProvider>
-);
+const AddColaborators = () => {
+  const { t } = useTranslation();
+  return (
+    <ThemeProvider theme={altTheme}>
+      <ViewContentBase>
+        <Typography
+          variant='h1'
+          sx={{
+            color: (theme) => theme.palette.text.primary,
+            marginBottom: '8.5rem',
+          }}
+        >
+          {t('AddColaborators.title')}
+        </Typography>
+      </ViewContentBase>
+    </ThemeProvider>
+  );
+};
 
 export default AddColaborators;
