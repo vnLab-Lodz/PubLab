631d0ef9c125196d1defd8825ed33d5cf2f9948c
karczewskiPiotr <karczewskiPiotr@users.noreply.github.com>
2021-12-08 22:28:22 +0000

    Prettified Code!

--- src/renderer/views/TechnologyPicker/TechnologiesPicker.tsx
+++ src/renderer/views/TechnologyPicker/TechnologiesPicker.tsx
@@ -4,22 +4,36 @@ import StyledSwitch from '../../components/Switch/Switch';
 import './TechnologiesPicker.scss';
 
 const TechnologiesPicker = () => {
-    const { t, i18n } = useTranslation();
-    let [ useSCSS, changeUseSCSS ] = useState(true);
-    let [ useTP, changeTP ] = useState(true);
+  const { t, i18n } = useTranslation();
+  let [useSCSS, changeUseSCSS] = useState(true);
+  let [useTP, changeTP] = useState(true);
 
-    return (<div className='container'>
-        <h2>{t('technology-picker.message')}</h2>
-        
-        <div className='switches'>
-        <div className='switch'><StyledSwitch checked={useSCSS} onChange={() => {
-            changeUseSCSS(useSCSS = !useSCSS);
-        }}/><p className='inline'> SCSS</p></div>
-        <div><StyledSwitch checked={useTP} onChange={() => {
-            changeTP(useTP = !useTP);
-        }}/><p className='inline'> TYPESCRIPT</p ></div>
+  return (
+    <div className='container'>
+      <h2>{t('technology-picker.message')}</h2>
+
+      <div className='switches'>
+        <div className='switch'>
+          <StyledSwitch
+            checked={useSCSS}
+            onChange={() => {
+              changeUseSCSS((useSCSS = !useSCSS));
+            }}
+          />
+          <p className='inline'> SCSS</p>
         </div>
-    </div>);
-}
+        <div>
+          <StyledSwitch
+            checked={useTP}
+            onChange={() => {
+              changeTP((useTP = !useTP));
+            }}
+          />
+          <p className='inline'> TYPESCRIPT</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default TechnologiesPicker;
