23dbaf416f48cef5a0a1cf377b604f31e252de86
Kuba <kubajaru@gmail.com>
2021-12-09 19:34:40 +0100

    Remove useStyle. Add use of addPublicationSlice

--- src/renderer/views/TechnologyPicker/TechnologiesPicker.tsx
+++ src/renderer/views/TechnologyPicker/TechnologiesPicker.tsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import { newPublication, setPublicationField } from '../../../shared/redux/slices/addPublicationSlice';
 import StyledSwitch from '../../components/Switch/Switch';
 import './TechnologiesPicker.scss';
 
 const TechnologiesPicker = () => {
-  const { t, i18n } = useTranslation();
-  let [useSCSS, changeUseSCSS] = useState(true);
-  let [useTP, changeTP] = useState(true);
+  const { t } = useTranslation();
+  const { useSass, useTypescript } = useSelector(newPublication);
+  const dispatch = useDispatch();
+
+  const toggleSass = () => {
+    dispatch(setPublicationField({ field: 'useSass', value: !useSass }));
+  };
+
+  const toggleTypescript = () => {
+    dispatch(setPublicationField({ field: 'useTypescript', value: !useTypescript }));
+  };
 
   return (
     <div className='container'>
@@ -15,21 +25,17 @@ const TechnologiesPicker = () => {
       <div className='switches'>
         <div className='switch'>
           <StyledSwitch
-            size='small'
-            checked={useSCSS}
-            onChange={() => {
-              changeUseSCSS((useSCSS = !useSCSS));
-            }}
+           size='small'
+           checked={useSass}
+           onChange={toggleSass}
           />
           <p className='inline'> SCSS</p>
         </div>
         <div>
           <StyledSwitch
-            size='small'
-            checked={useTP}
-            onChange={() => {
-              changeTP((useTP = !useTP));
-            }}
+           size='small'
+           checked={useTypescript}
+           onChange={toggleTypescript}
           />
           <p className='inline'> TYPESCRIPT</p>
         </div>
