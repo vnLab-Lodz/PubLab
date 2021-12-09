e78dc56b14464b3921027258917f95c44d31b2eb
wojtas112 <wojtas112@users.noreply.github.com>
2021-12-09 10:32:53 +0000

    Prettified Code!

--- src/shared/redux/slices/gatsbyGenerateProjectSlice.test.ts
+++ src/shared/redux/slices/gatsbyGenerateProjectSlice.test.ts
@@ -1,28 +1,28 @@
 import reducer, {
-    creationExecuting,
-    creationFulfilled,
-    creationRejected
+  creationExecuting,
+  creationFulfilled,
+  creationRejected,
 } from './gatsbyGenerateProjectSlice';
 
 describe('gatsbyGenerateProjectSlice', () => {
-    it('handles creationExecuting action', () => {
-      expect(reducer(undefined, creationExecuting())).toEqual({
-        creating: true,
-        creationSuccess: null,
-      });
+  it('handles creationExecuting action', () => {
+    expect(reducer(undefined, creationExecuting())).toEqual({
+      creating: true,
+      creationSuccess: null,
     });
+  });
 
-    it('handles creationFulfilled action', () => {
-      expect(reducer(undefined, creationFulfilled())).toEqual({
-        creating: false,
-        creationSuccess: true,
-      });
+  it('handles creationFulfilled action', () => {
+    expect(reducer(undefined, creationFulfilled())).toEqual({
+      creating: false,
+      creationSuccess: true,
     });
+  });
 
-    it('handles creationRejected action', () => {
-      expect(reducer(undefined, creationRejected())).toEqual({
-        creating: false,
-        creationSuccess: false,
-      });
+  it('handles creationRejected action', () => {
+    expect(reducer(undefined, creationRejected())).toEqual({
+      creating: false,
+      creationSuccess: false,
     });
+  });
 });
