27adcb9f3b0f16efbfaa8208d6c684666be6da9f
MateuszJaksa <jaksamateusz@gmail.com>
2021-12-11 01:05:30 +0100

    added tests to addPublicationSlice

---
+++ src/shared/redux/slices/addPublicationSlice.test.ts
@@ -0,0 +1,144 @@
+import reducer, {
+  deleteDraft,
+  setPublicationField,
+  addCollaborator,
+  deleteCollaborator,
+  increaseStep,
+  decreaseStep,
+  PublicationModification,
+  NewPublication,
+} from './addPublicationSlice';
+import { Collaborator } from './loadPublicationsSlice';
+
+describe('addPublicationSlice', () => {
+  it('handles deleteDraft action', () => {
+    expect(reducer(undefined, deleteDraft())).toEqual({
+      publicationName: '',
+      description: '',
+      collaborators: [],
+      packageManager: '',
+      useSass: false,
+      useTypescript: false,
+      step: 1,
+    });
+  });
+
+  it('handles setPublicationField action', () => {
+    const initialState: NewPublication = {
+      publicationName: '',
+      description: '',
+      collaborators: [],
+      packageManager: '',
+      useSass: false,
+      useTypescript: false,
+      step: 1,
+    };
+    const pubMob: PublicationModification = {
+      field: 'useSass',
+      value: true,
+    };
+    expect(reducer(initialState, setPublicationField(pubMob))).toEqual({
+      publicationName: '',
+      description: '',
+      collaborators: [],
+      packageManager: '',
+      useSass: true,
+      useTypescript: false,
+      step: 1,
+    });
+  });
+
+  it('handles addCollaborator action', () => {
+    const collaborator: Collaborator = {
+      id: 'id',
+      githubUsername: 'github_user',
+      role: 'role',
+    };
+    const initialState: NewPublication = {
+      publicationName: '',
+      description: '',
+      collaborators: [],
+      packageManager: '',
+      useSass: false,
+      useTypescript: false,
+      step: 1,
+    };
+    expect(reducer(initialState, addCollaborator(collaborator))).toEqual({
+      publicationName: '',
+      description: '',
+      collaborators: [collaborator],
+      packageManager: '',
+      useSass: false,
+      useTypescript: false,
+      step: 1,
+    });
+  });
+
+  it('handles deleteCollaborator action', () => {
+    const collaborator: Collaborator = {
+      id: 'id',
+      githubUsername: 'github_user',
+      role: 'role',
+    };
+    const initialState: NewPublication = {
+      publicationName: '',
+      description: '',
+      collaborators: [collaborator],
+      packageManager: '',
+      useSass: false,
+      useTypescript: false,
+      step: 1,
+    };
+    expect(reducer(initialState, deleteCollaborator('id'))).toEqual({
+      publicationName: '',
+      description: '',
+      collaborators: [],
+      packageManager: '',
+      useSass: false,
+      useTypescript: false,
+      step: 1,
+    });
+  });
+
+  it('handles increaseStep action', () => {
+    const initialState: NewPublication = {
+      publicationName: '',
+      description: '',
+      collaborators: [],
+      packageManager: '',
+      useSass: false,
+      useTypescript: false,
+      step: 3,
+    };
+    expect(reducer(initialState, increaseStep())).toEqual({
+      publicationName: '',
+      description: '',
+      collaborators: [],
+      packageManager: '',
+      useSass: false,
+      useTypescript: false,
+      step: 4,
+    });
+  });
+
+  it('handles decreaseStep action', () => {
+    const initialState: NewPublication = {
+      publicationName: '',
+      description: '',
+      collaborators: [],
+      packageManager: '',
+      useSass: false,
+      useTypescript: false,
+      step: 3,
+    };
+    expect(reducer(initialState, decreaseStep())).toEqual({
+      publicationName: '',
+      description: '',
+      collaborators: [],
+      packageManager: '',
+      useSass: false,
+      useTypescript: false,
+      step: 2,
+    });
+  });
+});
