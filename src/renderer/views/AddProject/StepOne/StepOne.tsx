317e46c778394c6d37fc998c1cc0d164f1215fa6
224094 Lemiesz Aleksander <224094@edu.p.lodz.pl>
2021-12-15 20:10:56 +0100

    Added internationalization to stepOne

--- src/renderer/views/AddProject/StepOne/StepOne.tsx
+++ src/renderer/views/AddProject/StepOne/StepOne.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable prettier/prettier */
 import React, { useState } from "react";
 import ImagePicker from '../../../components/ImagePicker/ImagePicker';
 import TextField from '../../../components/TextField/TextField';
 import TextArea from '../../../components/TextArea/TextArea';
 import InputLabel from '../../../components/InputLabel/InputLabel';
 import './StepOne.scss'
+import { useTranslation } from "react-i18next";
 
 
 
@@ -12,6 +14,7 @@ export default function StepOne() {
     const [imagePickerError, setImagePickerError] = useState(false)
     const [nameInputError, setNameInputError] = useState(false)
     const [descInputError, setDescInputError] = useState(false)
+    const { t } = useTranslation();
 
     const handleImageValidation = () => {
         setImagePickerError(true)
@@ -29,14 +32,14 @@ export default function StepOne() {
     return (
         <div className="grid-container">
             <div className="left-column">
-                <InputLabel error={imagePickerError} typographyVariant='h3'>PROJECT PHOTO</InputLabel>
+                <InputLabel error={imagePickerError} typographyVariant='h3'>{t('StepOne.projectPhoto')}</InputLabel>
                 <ImagePicker error={imagePickerError} onClick={() => handleImageValidation()} />
             </div>
             <div className="right-column">
-                <InputLabel error={nameInputError} typographyVariant='h3'>PROJECT NAME</InputLabel>
-                <TextField className="name-input-field" placeholder="Project name..." error={nameInputError} onChange={(e) => handleNameValidation(e.target.value)} />
-                <InputLabel error={descInputError} typographyVariant='h3'>PROJECT DECRIPTION</InputLabel>
-                <TextArea className="desc-input-field" placeholder="Project description..." error={descInputError} onChange={(e) => handleDescValidation(e.target.value)} />
+                <InputLabel error={nameInputError} typographyVariant='h3'>{t('StepOne.projectName')}</InputLabel>
+                <TextField className="name-input-field" placeholder={t('StepOne.projectNameDetails')} error={nameInputError} onChange={(e) => handleNameValidation(e.target.value)} />
+                <InputLabel error={descInputError} typographyVariant='h3'>{t('StepOne.projectDescription')}</InputLabel>
+                <TextArea className="desc-input-field" placeholder={t('StepOne.projectDescriptionDetails')} error={descInputError} onChange={(e) => handleDescValidation(e.target.value)} />
             </div>
         </div>
     )
