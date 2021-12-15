65dba239693c4a20e052f196d0600033dffa524c
zucchero2304 <remek.piwowarski@gmail.com>
2021-12-15 21:09:00 +0100

    Added button activation/deactivation

--- src/renderer/views/AddProject/StepOne/StepOne.tsx
+++ src/renderer/views/AddProject/StepOne/StepOne.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ImagePicker from '../../../components/ImagePicker/ImagePicker';
 import TextField from '../../../components/TextField/TextField';
 import TextArea from '../../../components/TextArea/TextArea';
@@ -7,27 +7,39 @@ import InputLabel from '../../../components/InputLabel/InputLabel';
 import './StepOne.scss'
 import { useTranslation } from "react-i18next";
 
-
-
-export default function StepOne() {
+export default function StepOne(props: any) {
 
     const [imagePickerError, setImagePickerError] = useState(false)
     const [nameInputError, setNameInputError] = useState(false)
     const [descInputError, setDescInputError] = useState(false)
+
+    const [imageValidationTriggered, setImageValidationTriggered] = useState(false)
+    const [nameValidationTriggered, setNameValidationTriggered] = useState(false)
+    const [descValidationTriggered, setDescValidationTriggered] = useState(false)
+
     const { t } = useTranslation();
 
     const handleImageValidation = () => {
         setImagePickerError(true)
+        setImageValidationTriggered(true)
     }
 
     const handleNameValidation = (input: string) => {
         input === '' ? setNameInputError(true) : setNameInputError(false)
+        setNameValidationTriggered(true)
     }
 
     const handleDescValidation = (input: string) => {
         input === '' ? setDescInputError(true) : setDescInputError(false)
-
+        setDescValidationTriggered(true)
     }
+
+    useEffect(() => {
+        //add check for imageValidationTriggered after adding image will be handled (now clicking doesn't open img selection dialog)
+        if (nameValidationTriggered && descValidationTriggered) {
+            (imagePickerError || nameInputError || descInputError) ? props.setNextButtonEnabled(false) : props.setNextButtonEnabled(true)
+        }
+    }, [imagePickerError, nameInputError, descInputError])
 
     return (
         <div className="grid-container">
