387f79c3547fd5f52a0bff7cd717963ff01340a0
LostInStatic <kowal.a1234@gmail.com>
2022-03-12 18:38:49 +0100

    feat: :lipstick: implement the package manager picker UI

---
+++ src/renderer/views/AddProject/subcomponents/PackageManagerPicker/PackageManagerPicker.tsx
@@ -0,0 +1,53 @@
+import { Box, FormControlLabel, RadioGroup, Typography } from '@mui/material';
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  NewPublication,
+  newPublication,
+  setPublicationField,
+} from '../../../../../shared/redux/slices/addPublicationSlice';
+import RadioBtn from '../../../../components/RadioButton/RadioBtn';
+
+const PackageManagerPicker = () => {
+  const { t } = useTranslation();
+  const { packageManager } = useSelector(newPublication);
+  const dispatch = useDispatch();
+
+  const setPackageManager = (value: NewPublication['packageManager']) => {
+    dispatch(setPublicationField({ field: 'packageManager', value }));
+  };
+
+  return (
+    <Box>
+      <Typography variant='subtitle1' component='p' mb={3}>
+        {t('AddProject.PackageManager.message')}
+      </Typography>
+
+      <Box mb={2}>
+        <RadioGroup
+          defaultValue={packageManager}
+          onChange={(e) =>
+            setPackageManager(
+              e.target.value as NewPublication['packageManager']
+            )
+          }
+        >
+          <FormControlLabel
+            value='npm'
+            control={<RadioBtn />}
+            label={<Typography variant='body2'>NPM</Typography>}
+          />
+          <FormControlLabel
+            disabled
+            value='yarn'
+            control={<RadioBtn />}
+            label={<Typography variant='body2'>YARN</Typography>}
+          />
+        </RadioGroup>
+      </Box>
+    </Box>
+  );
+};
+
+export default PackageManagerPicker;
