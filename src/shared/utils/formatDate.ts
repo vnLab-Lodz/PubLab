b79dcd026de8ec2b194f3a3956c811a5fec0e555
LostInStatic <kowal.a1234@gmail.com>
2022-03-28 07:33:41 +0200

    refactor: :fire: remove unused file

--- src/shared/utils/formatDate.ts
+++
@@ -1,8 +0,0 @@
-export function formatDate(dateStr: string): string {
-  const date = new Date(dateStr);
-  const day = `0${date.getDate()}`.slice(-2);
-  const month = `0${date.getMonth() + 1}`.slice(-2);
-  const year = date.getFullYear();
-
-  return `${year}-${month}-${day}`;
-}
