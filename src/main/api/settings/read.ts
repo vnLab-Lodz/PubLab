c1c68662674bcdbc99e03ab052e0275804d29679
Piotr Karczewski <p.karczewsk@gmail.com>
2022-04-15 11:40:41 +0200

    fix: :bug: Use app settings defaults if the settings file is not present

--- src/main/api/settings/read.ts
+++ src/main/api/settings/read.ts
@@ -1,13 +1,20 @@
 import { mainStore as store } from 'src/main';
 import createFileIO from 'src/main/lib/fileIO';
+import { createLogger } from 'src/main/logger';
 import { SETTINGS_FILE_PATH } from 'src/shared/constants';
 import { setSettings, Settings } from 'src/shared/redux/slices/settingsSlice';
 import { IpcEventHandler } from 'src/shared/types/api';
 
 const read: IpcEventHandler = async () => {
   const io = createFileIO();
-  const nextSettings = await io.readJSON<Settings>(SETTINGS_FILE_PATH);
-  store.dispatch(setSettings(nextSettings));
+  const logger = createLogger();
+
+  try {
+    const nextSettings = await io.readJSON<Settings>(SETTINGS_FILE_PATH);
+    store.dispatch(setSettings(nextSettings));
+  } catch (error) {
+    logger.appendLog('No app settings found, using defaults.');
+  }
 };
 
 export default read;
