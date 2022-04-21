dc336f429a8de206cfa86b8b83efca5922788f57
Piotr Karczewski <p.karczewsk@gmail.com>
2022-04-21 11:51:09 +0200

    fix: :bug: Disallow reserved keywords and invalid chars in project name

--- src/renderer/views/AddProject/subcomponents/ProjectDetailsInput/validationSchema.ts
+++ src/renderer/views/AddProject/subcomponents/ProjectDetailsInput/validationSchema.ts
@@ -1,7 +1,19 @@
+/* eslint-disable no-control-regex */
 import { object, string, InferType } from 'yup';
 
+// Naming Files, Paths, and Namespaces:
+// https://docs.microsoft.com/pl-pl/windows/win32/fileio/naming-a-file?redirectedfrom=MSDN
+const invalidRegex =
+  /[<>:"/\\|?*\x00-\x1F]|^(?:aux|con|clock\$|nul|prn|com[1-9]|lpt[1-9])$/;
+
 const validationSchema = object({
-  name: string().required('common.field_required'),
+  name: string()
+    .test(
+      'is-valid-folder-name',
+      'common.invalid_folder_name',
+      (value) => !value?.match(invalidRegex)
+    )
+    .required('common.field_required'),
   description: string().max(220),
 });
 
