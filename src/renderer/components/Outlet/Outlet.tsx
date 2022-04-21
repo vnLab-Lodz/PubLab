d8aa518d326c5fd93a46f1edecb20e8d632e3d37
Piotr Karczewski <p.karczewsk@gmail.com>
2022-04-21 08:09:30 +0200

    refactor: :truck: Rename NotificationOutlet file
    index.tsx -> NotificationOutlet.tsx

--- src/renderer/components/Outlet/Outlet.tsx
+++ src/renderer/components/Outlet/Outlet.tsx
@@ -5,7 +5,7 @@ import CustomRouter from '../CustomRouter/CustomRouter';
 import * as Styled from './style';
 import { selectCurrentView } from '../../../shared/redux/slices/currentViewSlice';
 import { FULL_SCREEN_VIEWS } from '../../constants/RouterComponents';
-import NotificationsOutlet from '../NotificationsOutlet';
+import NotificationsOutlet from '../NotificationsOutlet/NotificationsOutlet';
 
 const Outlet = () => {
   const { view } = useSelector(selectCurrentView);
