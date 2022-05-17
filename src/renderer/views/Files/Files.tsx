4fc59d86047bcfbfa4c50fc3efcaef2b3b555da8
LostInStatic <kowal.a1234@gmail.com>
2022-05-18 01:13:15 +0200

    feat: :sparkles: first draft of the files view

--- src/renderer/views/Files/Files.tsx
+++ src/renderer/views/Files/Files.tsx
@@ -1,5 +1,49 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import path from 'path';
+import { TreeView } from '@mui/lab';
+import { ArrowDropDown, ArrowRight } from '@mui/icons-material';
+import { activePublication } from '../../../shared/redux/slices/loadPublicationsSlice';
+import { LocalPublication } from '../../../shared/types';
+import FileTreeItem from './subcomponents/FileTreeItem';
+import ViewContent from '../../components/ViewContent/ViewContent';
 
-const Files = () => <div>Files</div>;
+const Files = () => {
+  const project = useSelector(activePublication) as LocalPublication;
+  const [expanded, setExpanded] = React.useState<string[]>([]);
+  const [selected, setSelected] = React.useState<string[]>([]);
+
+  const handleToggle = (event: React.SyntheticEvent, nodeIds: string[]) => {
+    setExpanded(nodeIds);
+  };
+
+  const handleSelect = (event: React.SyntheticEvent, nodeIds: string[]) => {
+    setSelected(nodeIds);
+  };
+  return (
+    <ViewContent>
+      <TreeView
+        expanded={expanded}
+        selected={selected}
+        defaultCollapseIcon={<ArrowDropDown />}
+        defaultExpandIcon={<ArrowRight />}
+        onNodeToggle={handleToggle}
+        onNodeSelect={handleSelect}
+      >
+        <FileTreeItem
+          entry={{
+            name: project.name,
+            details: { isDirectory: true, content: undefined },
+          }}
+          dirPath={path.join(project.dirPath)}
+          depth={1}
+          preload
+          treeLevel={0}
+          expandedNodes={expanded}
+        />
+      </TreeView>
+    </ViewContent>
+  );
+};
 
 export default Files;
---
+++ src/renderer/views/Files/subcomponents/FileTreeItem.tsx
@@ -0,0 +1,91 @@
+import path from 'path';
+import React, { useEffect, useState } from 'react';
+import { DirectoryEntryInfo } from '../../../../shared/types/api';
+import { readDirectory } from '../../../ipc';
+import FileDisplay from '../../../components/FileDisplay/FileDisplay';
+import * as Styled from '../style';
+
+interface Props {
+  entry: DirectoryEntryInfo;
+  dirPath: string;
+  expandedNodes: string[];
+  treeLevel: number;
+  depth?: number;
+  preload?: boolean;
+}
+
+const FileTreeItem = ({
+  entry,
+  dirPath,
+  depth,
+  preload,
+  expandedNodes,
+  treeLevel,
+}: Props) => {
+  const dirContent = entry.details.isDirectory
+    ? entry.details.content
+    : undefined;
+  const [isLoading, setIsLoading] = useState(false);
+  const [content, setContent] = useState(
+    [] as DirectoryEntryInfo[] | undefined
+  );
+
+  useEffect(() => {
+    if (entry.details.isDirectory && !dirContent && preload) {
+      setIsLoading(true);
+      readDirectory(dirPath, depth).then((value) => {
+        setContent(value);
+        setIsLoading(false);
+      });
+    } else setContent(dirContent);
+  }, [preload, dirContent]);
+
+  const renderContent = () =>
+    contentMap(content, {
+      dirPath,
+      depth,
+      expandedNodes,
+      treeLevel,
+    });
+
+  if (treeLevel === 0) return <>{isLoading ? 'Loading' : renderContent()}</>;
+  return (
+    <Styled.TreeItem
+      nodeId={dirPath}
+      treeLevel={treeLevel}
+      label={<FileDisplay entry={entry} treeLevel={treeLevel} />}
+    >
+      {isLoading ? 'Loading' : renderContent()}
+    </Styled.TreeItem>
+  );
+};
+
+FileTreeItem.defaultProps = {
+  depth: 1,
+  preload: false,
+};
+
+export default FileTreeItem;
+
+export function contentMap(
+  content: DirectoryEntryInfo[] | undefined,
+  props: Pick<Props, 'depth' | 'dirPath' | 'expandedNodes' | 'treeLevel'>
+) {
+  if (!content) return [];
+  return content.map((entry) => {
+    const entryPath = path.join(props.dirPath, entry.name);
+    const preload =
+      props.expandedNodes.includes(props.dirPath) || props.treeLevel === 0;
+    return (
+      <FileTreeItem
+        entry={entry}
+        key={entryPath}
+        depth={props.depth}
+        dirPath={entryPath}
+        preload={preload}
+        expandedNodes={props.expandedNodes}
+        treeLevel={props.treeLevel + 1}
+      />
+    );
+  });
+}
