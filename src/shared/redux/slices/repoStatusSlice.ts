b007ea84ef12dbdb8c7046c66198b7ee5dc155bc
LostInStatic <kowal.a1234@gmail.com>
2022-06-03 00:28:34 +0200

    refactor: :recycle: improve slice shape

--- src/shared/redux/slices/repoStatusSlice.ts
+++ src/shared/redux/slices/repoStatusSlice.ts
@@ -1,18 +1,26 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { GitRepoTreeItem } from '../../types/api';
+import { RootState } from '../rootReducer';
 
-type RepoStatus = GitRepoTreeItem | {};
+interface RepoStatus {
+  tree: GitRepoTreeItem | undefined;
+}
 
-const initialState: RepoStatus = {};
+const initialState: RepoStatus = { tree: undefined };
 
 const repoStatusSlice = createSlice({
   name: 'repoStatus',
   initialState,
   reducers: {
-    setStatusTree: (_, action: PayloadAction<RepoStatus>) => action.payload,
+    setStatusTree: (state, action: PayloadAction<RepoStatus['tree']>) => ({
+      ...state,
+      tree: action.payload,
+    }),
   },
 });
 
 export const { setStatusTree } = repoStatusSlice.actions;
+
+export const selectRepoTree = (state: RootState) => state.repoStatus.tree;
 
 export default repoStatusSlice.reducer;
