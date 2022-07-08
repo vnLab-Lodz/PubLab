a7bca5adbd2083cfda648f84ba8c5bbb9941917b
LostInStatic <kowal.a1234@gmail.com>
2022-07-08 03:02:09 +0200

    feat: :globe_with_meridians: add translations

--- src/renderer/views/ProjectsList/subcomponents/ProjectSearch/ProjectSearch.tsx
+++ src/renderer/views/ProjectsList/subcomponents/ProjectSearch/ProjectSearch.tsx
@@ -1,5 +1,6 @@
 import { Autocomplete } from '@mui/material';
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import { Publication } from '../../../../../shared/types';
 import SearchField from '../../../../components/SearchField/SearchField';
 
@@ -8,16 +9,27 @@ interface Props {
   onChange: (terms: string[]) => void;
 }
 
-const ProjectSearch: React.FC<Props> = ({ publications, onChange }) => (
-  <Autocomplete
-    options={getAutocompleteOptions(publications)}
-    onChange={(_, value) => onChange(value)}
-    renderInput={(params) => <SearchField {...params} />}
-    size='small'
-    freeSolo
-    multiple
-  />
-);
+const ProjectSearch: React.FC<Props> = ({ publications, onChange }) => {
+  const { t } = useTranslation();
+  return (
+    <Autocomplete
+      options={getAutocompleteOptions(publications)}
+      onChange={(_, value) => onChange(value)}
+      renderInput={(params) => (
+        <SearchField
+          {...params}
+          InputProps={{
+            'aria-label': t('common.search'),
+            ...params.InputProps,
+          }}
+        />
+      )}
+      size='small'
+      freeSolo
+      multiple
+    />
+  );
+};
 
 export default ProjectSearch;
 
