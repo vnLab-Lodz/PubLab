93d94b82973b25905e682d7239d4ea92f51dd2dc
LostInStatic <kowal.a1234@gmail.com>
2022-07-27 00:05:56 +0200

    feat: :speech_balloon: improve commit message

--- src/renderer/views/Settings/Settings.tsx
+++ src/renderer/views/Settings/Settings.tsx
@@ -85,7 +85,7 @@ const Settings = () => {
               const id = uuidv4();
               setLoaderID(id);
               await updateConfig(project.dirPath, changes);
-              await gitCommit('config changes');
+              await gitCommit('Config update\n\n[publAB automatic commit]');
               await gitPush(id);
               dispatch(updatePublication({ ...project, ...changes }));
               dispatch(
