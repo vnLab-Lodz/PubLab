c2b25a039f1abc8c02203232fec79714904ddb96
Piotr Karczewski <p.karczewsk@gmail.com>
2022-09-21 15:14:20 +0100

    feat: :sparkles: Use proper templates based on creation options
    - add step to allow for choosing between single/mulit language projects
    - show proper tooltips to explain disabled options across steps

--- src/main/lib/gatsbyProjectGenerator.ts
+++ src/main/lib/gatsbyProjectGenerator.ts
@@ -1,6 +1,6 @@
-import { PublicationBase } from 'src/shared/types';
 import child_process from 'child_process';
 import util from 'util';
+import { AddPublicationWizard } from 'src/shared/redux/slices/addPublicationWizardSlice';
 import { createLogger } from '../logger';
 
 const exec = util.promisify(child_process.exec);
@@ -8,6 +8,7 @@ const exec = util.promisify(child_process.exec);
 export enum TEMPLATE_URLS {
   PAAW_I18N = 'https://github.com/vnLab-Lodz/gatsby-starter-paaw-i18n',
   PAAW_BASIC = 'https://github.com/vnLab-Lodz/gatsby-starter-paaw-basic',
+  PAAW_SASS = 'https://github.com/vnLab-Lodz/gatsby-starter-paaw-sass',
 }
 
 export interface GatsbyProjectGenerator {
@@ -16,14 +17,13 @@ export interface GatsbyProjectGenerator {
 }
 
 const createGatsbyProjectGenerator = (
-  publication: PublicationBase
+  publication: AddPublicationWizard['data']
 ): GatsbyProjectGenerator => ({
   getTemplateUrl() {
-    const { useSass, useTypescript } = publication;
+    const { useSass, multilingual } = publication;
+    if (multilingual) return TEMPLATE_URLS.PAAW_I18N;
 
-    return useSass && useTypescript
-      ? TEMPLATE_URLS.PAAW_I18N
-      : TEMPLATE_URLS.PAAW_BASIC;
+    return useSass ? TEMPLATE_URLS.PAAW_SASS : TEMPLATE_URLS.PAAW_BASIC;
   },
   async generate(cwd, repoName = publication.name) {
     const logger = createLogger();
--- src/renderer/components/TechnologiesPicker/TechnologiesPicker.tsx
+++ src/renderer/components/TechnologiesPicker/TechnologiesPicker.tsx
@@ -1,11 +1,14 @@
 import { Box, Typography } from '@mui/material';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { Publication } from '../../../shared/types';
+import { AddPublicationWizard } from 'src/shared/redux/slices/addPublicationWizardSlice';
 import StyledSwitch from '../Switch/Switch';
 import Tooltip from '../Tooltip/Tooltip';
 
-type State = Pick<Publication, 'useSass' | 'useTypescript'>;
+type State = Pick<
+  AddPublicationWizard['data'],
+  'useSass' | 'useTypescript' | 'multilingual'
+>;
 
 interface Props {
   onSubmit: (state: State) => void;
@@ -15,9 +18,16 @@ interface Props {
 const TechnologiesPicker = ({ onSubmit, state }: Props) => {
   const { t } = useTranslation();
 
+  const sassTooltip = state.multilingual
+    ? t('technology-picker.sass-tooltip')
+    : '';
+
+  const sassLabelColor = state.multilingual ? 'gray.dark' : undefined;
+
   const toggleSass = () => {
     onSubmit({ ...state, useSass: !state.useSass });
   };
+
   const toggleTypescript = () => {
     onSubmit({ ...state, useTypescript: !state.useTypescript });
   };
@@ -30,14 +40,19 @@ const TechnologiesPicker = ({ onSubmit, state }: Props) => {
 
       <Box>
         <Box mb={2}>
-          <StyledSwitch
-            size='small'
-            checked={state.useSass}
-            onChange={toggleSass}
-          />
-          <Typography variant='body2' ml={1}>
-            SCSS
-          </Typography>
+          <Tooltip title={sassTooltip} arrow placement='top-start'>
+            <span>
+              <StyledSwitch
+                size='small'
+                checked={state.multilingual || state.useSass}
+                onChange={toggleSass}
+                disabled={state.multilingual}
+              />
+              <Typography variant='body2' ml={1} color={sassLabelColor}>
+                SCSS
+              </Typography>
+            </span>
+          </Tooltip>
         </Box>
         <Box>
           <Tooltip
